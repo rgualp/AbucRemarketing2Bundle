89f0c27986408ba3fe22269198cc57e0b434c147
Jakob <deadzac@gmx.net>
2013-09-03 17:35:14 +0200

    Added Skrill helper. General update.

--- src/MyCp/frontEndBundle/Controller/paymentController.php
+++ src/MyCp/frontEndBundle/Controller/paymentController.php
@@ -2,38 +2,44 @@
 
 namespace MyCp\FrontendBundle\Controller;
 
-
+use Doctrine\ORM\EntityNotFoundException;
+use MyCp\frontEndBundle\Helpers\SkrillStatusResponse;
+use MyCp\mycpBundle\Entity\generalReservation;
+use MyCp\mycpBundle\Entity\user;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class paymentController extends Controller {
 
-    public function skrillAction()
+    public function skrillAction($reservationId = 0)
     {
+        $em = $this->getDoctrine()->getManager();
+        $reservation = $em->find('generalReservation', $reservationId);
+
+        if(empty($reservation) || !($reservation instanceof generalReservation)) {
+            throw new EntityNotFoundException("generalReservation($reservationId)");
+        }
+
+        $user = $reservation->getUser();
+
+        if(empty($user) || !($user instanceof user)) {
+            throw new EntityNotFoundException("user($userId)");
+        }
+
+        $loggedInUser = $this->get('security.context')->getToken()->getUser();
+
+        if($user->getUserId() !== $loggedInUser->getUserId()) {
+            throw new AuthenticationException('Access to resource not permitted.');
+        }
+
+        $country = $user->getCountry()->getCode();
 
         // TODO: get all the booking information from DB and publish to view
         //$url = $this->generateUrl('skrillReturn');
-        $skrillData = array(
-            'name' => 'Jakob',
-            'action_url' => 'https://www.moneybookers.com/app/payment.pl',
-            'pay_to_email' => 'a@b.de', // ABUC email
-            'recipient_description' => 'MyCasaParticular',
-            'transaction_id' => 'abuc_transaction_id',
-            'return_url' => $this->generateUrl('frontend_payment_skrill_return', array(), true),
-            'return_url_text' => 'Return to MyCasaParticular',
-            'cancel_url' => $this->generateUrl('frontend_payment_skrill_cancel', array(), true),
-            'status_url' => $this->generateUrl('frontend_payment_skrill_status', array(), true),
-            'status_url2' => 'booking@mycasaparticular.com',
-            'language' => 'EN',
-            'confirmation_note' => 'Thank you for booking with MyCasaParticular!',
-            'pay_from_email' => 'customer@customer.com', // customer email
-            'logo_url' => '',
 
-
-
-    );
-
+        $skrillData = $this->getSkrillViewData($reservation, $user);
 
         return $this->render('frontEndBundle:Payment:skrill.html.twig', $skrillData);
     }
@@ -46,13 +52,16 @@ class paymentController extends Controller {
     public function skrillStatusAction()
     {
 
-        $request = $this->getRequest()->request;
-        $email = $request->get('pay_to_email'); //all();
+        $request = $this->getRequest()->request->all();
+        $skrillRequest = new SkrillStatusResponse($request);
 
         ob_start();
-        var_dump($request);
+        //var_dump($request);
+        var_dump($skrillRequest);
         $dump = ob_get_clean();
 
+        // TODO: write answer POST values to database and return http status 200
+        //return new Response('', 200);
         return $this->render(
             'frontEndBundle:Payment:skrill.html.twig',
             array('name' => 'Return', 'postRequest' => $dump));
@@ -64,5 +73,43 @@ class paymentController extends Controller {
         return $this->render(
             'frontEndBundle:Payment:skrill.html.twig',
             array('name' => 'Cancel'));
+    }
+
+    private function getSkrillViewData(generalReservation $reservation, user $user)
+    {
+
+        return array(
+            'action_url' => 'https://www.moneybookers.com/app/payment.pl',
+            'pay_to_email' => 'a@b.de', // ABUC email
+            'recipient_description' => 'MyCasaParticular.com',
+            'transaction_id' => 'abuc_transaction_id',
+            'return_url' => $this->generateUrl('frontend_payment_skrill_return', array(), true),
+            'return_url_text' => 'Return to MyCasaParticular',
+            'cancel_url' => $this->generateUrl('frontend_payment_skrill_cancel', array(), true),
+            'status_url' => $this->generateUrl('frontend_payment_skrill_status', array(), true),
+            'status_url2' => 'booking@mycasaparticular.com',
+            'language' => 'EN',
+            'confirmation_note' => 'MyCasaParticular wishes you pleasure visiting Cuba!',
+            'pay_from_email' => 'customer@lkjldasjfljkadf.com', // customer email
+            'logo_url' => '',//'http://www.mypaladar.com/mycp/mycpres/web/bundles/frontend/images/logo.png', // TODO: $this->getRequest()->getUriForPath('bundles/frontend/images/logo.png') //;'bundles/frontend/images/logo.png',
+            'first_name' => 'John',
+            'last_name' => 'Payer',
+            'address' => '11 Payerstr St',
+            'postal_code' => 'EC45MQ',
+            'city' => 'Payertown',
+            'country' => 'GBR', // TODO: wo die Liste der erlaubten countries speichern?! -> country überhaupt nötig?!
+            'amount' => '0.75',
+            'currency' => 'EUR', // TODO: Liste der Währungen
+            'detail1_description' => 'ID Oferta:  ',
+            'detail1_text' => 'CAS.1000xxx',
+            'detail2_description' => 'Casa:  ',
+            'detail2_text' => '1234',
+            'detail3_description' => 'Description:',
+            'detail3_text' => 'Rooms: 1 - Arrival: 10.10.2013 - Nights: 5',
+            'detail4_description' => 'Description:',
+            'detail4_text' => 'Adults: 2 - Children: 1',
+            'payment_methods' => 'ACC,DID,SFT',
+            'button_text' => 'Pay With Skrill'
+        );
     }
 }
---
+++ src/MyCp/frontEndBundle/Helpers/SkrillHelper.php
@@ -0,0 +1,478 @@
+<?php
+
+namespace MyCp\frontEndBundle\Helpers;
+
+final class SkrillHelper
+{
+    private static $statusCodeMap = array(
+        '2' => 'Processed',
+        '0' => 'Pending',
+        '-1' => 'Cancelled',
+        '-2' => 'Failed',
+        '-3' => 'Chargeback'
+    );
+
+    private static $failureCodeMap = array(
+        '01' => 'Referred',
+        '02' => 'Invalid Merchant Number',
+        '03' => 'Pick-up card',
+        '04' => 'Authorisation Declined',
+        '05' => 'Other Error',
+        '06' => 'CVV is mandatory, but not set or invalid',
+        '07' => 'Approved authorisation, honour with identification',
+        '08' => 'Delayed Processing',
+        '09' => 'Invalid Transaction',
+        '10' => 'Invalid Currency',
+        '11' => 'Invalid Amount/Available Limit Exceeded/Amount too high',
+        '12' => 'Invalid credit card or bank account',
+        '13' => 'Invalid Card Issuer',
+        '14' => 'Annulation by client',
+        '15' => 'Duplicate transaction',
+        '16' => 'Acquirer Error',
+        '17' => 'Reversal not processed, matching authorisation not found',
+        '18' => 'File Transfer not available/unsuccessful',
+        '19' => 'Reference number error',
+        '20' => 'Access Denied',
+        '21' => 'File Transfer failed',
+        '22' => 'Format Error',
+        '23' => 'Unknown Acquirer',
+        '24' => 'Card expired',
+        '25' => 'Fraud Suspicion',
+        '26' => 'Security code expired',
+        '27' => 'Requested function not available',
+        '28' => 'Lost/Stolen card',
+        '29' => 'Stolen card, Pick up',
+        '30' => 'Duplicate Authorisation',
+        '31' => 'Limit Exceeded',
+        '32' => 'Invalid Security Code',
+        '33' => 'Unknown or Invalid Card/Bank account',
+        '34' => 'Illegal Transaction',
+        '35' => 'Transaction Not Permitted',
+        '36' => 'Card blocked in local blacklist',
+        '37' => 'Restricted card/bank account',
+        '38' => 'Security Rules Violation',
+        '39' => 'The transaction amount of the referencing transaction is higher than the transaction amount of the original transaction Transaction frequency limit exceeded, override is possible',
+        '40' => 'Transaction frequency limit exceeded, override is possible',
+        '41' => 'Incorrect usage count in the Authorisation System exceeded',
+        '42' => 'Card blocked',
+        '43' => 'Rejected by Credit Card Issuer',
+        '44' => 'Card Issuing Bank or Network is not available',
+        '45' => 'The card type is not processed by the authorisation centre / Authorisation System has determined incorrect Routing Processing temporarily not possible',
+        '46' => 'undefined',
+        '47' => 'Processing temporarily not possible',
+        '48' => 'Security Breach',
+        '49' => 'Date / time not plausible, trace-no. not increasing',
+        '50' => 'Error in PAC encryption detected',
+        '51' => 'System Error',
+        '52' => 'MB Denied - potential fraud',
+        '53' => 'Mobile verification failed',
+        '54' => 'Failed due to internal security restrictions',
+        '55' => 'Communication or verification problem',
+        '56' => '3D verification failed',
+        '57' => 'AVS check failed',
+        '58' => 'Invalid bank code',
+        '59' => 'Invalid account code',
+        '60' => 'Card not authorised',
+        '61' => 'No credit worthiness',
+        '62' => 'Communication error',
+        '63' => 'Transaction not allowed for cardholder',
+        '64' => 'Invalid Data in Request',
+        '65' => 'Blocked bank code',
+        '66' => 'CVV2/CVC2 Failure',
+        '99' => 'General error',
+    );
+
+
+    /**
+     * Hide constructor as the class only contains static methods.
+     */
+    private function __construct()
+    {
+
+    }
+
+    /**
+     * Returns a description for a Skrill status code.
+     *
+     * @param string $skrillStatusCode The status code.
+     * @return string The description.
+     */
+    public static function getSkrillStatusCodeDescrition($skrillStatusCode)
+    {
+        if(isset(self::$statusCodeMap[$skrillStatusCode])) {
+            return self::$statusCodeMap[$skrillStatusCode];
+        }
+
+        return 'unknown status';
+    }
+
+    /**
+     * Returns the failure description belonging to a Skrill failure code.
+     * (Taken from the Moneybookers Gateway Manual, v.6.18)
+     *
+     * @param string $skrillFailureCode The Skrill failure code.
+     * @return string The failure code description.
+     */
+    public static function getSkrillFailureDescription($skrillFailureCode)
+    {
+        if(isset(self::$failureCodeMap[$skrillFailureCode])) {
+            return self::$failureCodeMap[$skrillFailureCode];
+        }
+
+        return 'unknown failure code';
+    }
+}
+
+class SkrillStatusResponse
+{
+    private $pay_to_email;
+    private $pay_from_email;
+    private $merchant_id;
+    private $customer_id;
+    private $merchant_transaction_id;
+    private $skrill_transaction_id;
+    private $payed_amount;
+    private $skrill_currency;
+    private $status;
+    private $failed_reason_code;
+    private $failed_reason_description;
+    private $md5_signature;
+    private $merchant_amount;
+    private $merchant_currency;
+    private $payment_type;
+    private $merchant_fields;
+
+
+    public function __construct(array $skrillStatusResponse = array())
+    {
+        $this->initializeWithSkrillResponse($skrillStatusResponse);
+    }
+
+    /**
+     * Takes the http POST status request array transmitted by the Skrill service
+     * and fills the members.
+     *
+     * @param array $skrillStatusRequest The Skrill status request.
+     */
+    public function initializeWithSkrillResponse(array $skrillStatusRequest = array())
+    {
+        if(isset($skrillStatusRequest['pay_to_email'])) {
+            $this->setPayToEmail($skrillStatusRequest['pay_to_email']);
+        }
+
+        if(isset($skrillStatusRequest['pay_from_email'])) {
+            $this->setPayFromEmail($skrillStatusRequest['pay_from_email']);
+        }
+
+        if(isset($skrillStatusRequest['merchant_id'])) {
+            $this->setMerchantId($skrillStatusRequest['merchant_id']);
+        }
+
+        if(isset($skrillStatusRequest['customer_id'])) {
+            $this->setCustomerId($skrillStatusRequest['customer_id']);
+        }
+
+        if(isset($skrillStatusRequest['transaction_id'])) {
+            $this->setMerchantTransactionId($skrillStatusRequest['transaction_id']);
+        }
+
+        if(isset($skrillStatusRequest['mb_transaction_id'])) {
+            $this->setSkrillTransactionId($skrillStatusRequest['mb_transaction_id']);
+        }
+
+        if(isset($skrillStatusRequest['mb_amount'])) {
+            $this->setPayedAmount($skrillStatusRequest['mb_amount']);
+        }
+
+        if(isset($skrillStatusRequest['mb_currency'])) {
+            $this->setSkrillCurrency($skrillStatusRequest['mb_currency']);
+        }
+
+        if(isset($skrillStatusRequest['status'])) {
+            $this->setStatus($skrillStatusRequest['status']);
+        }
+
+        if(isset($skrillStatusRequest['failed_reason_code'])) {
+            $failedReasonCode = trim($skrillStatusRequest['failed_reason_code']);
+            $this->setFailedReasonCode($failedReasonCode);
+            $this->setFailedReasonDescription(SkrillHelper::getSkrillFailureDescription($failedReasonCode));
+        }
+
+        if(isset($skrillStatusRequest['md5sig'])) {
+            $this->setMd5Signature($skrillStatusRequest['md5sig']);
+        }
+
+        if(isset($skrillStatusRequest['amount'])) {
+            $this->setMerchantAmount($skrillStatusRequest['amount']);
+        }
+
+        if(isset($skrillStatusRequest['currency'])) {
+            $this->setMerchantCurrency($skrillStatusRequest['currency']);
+        }
+
+        if(isset($skrillStatusRequest['payment_type'])) {
+            $this->setPaymentType($skrillStatusRequest['payment_type']);
+        }
+
+        if(isset($skrillStatusRequest['merchant_fields'])) {
+            // TODO: split to array
+            $this->setMerchantFields($skrillStatusRequest['merchant_fields']);
+        }
+    }
+
+    /**
+     * @param mixed $failed_reason_code
+     */
+    public function setFailedReasonCode($failed_reason_code)
+    {
+        $this->failed_reason_code = $failed_reason_code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFailedReasonCode()
+    {
+        return $this->failed_reason_code;
+    }
+
+    /**
+     * @param string $failed_reason_description
+     */
+    public function setFailedReasonDescription($failed_reason_description)
+    {
+        $this->failed_reason_description = $failed_reason_description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFailedReasonDescription()
+    {
+        return $this->failed_reason_description;
+    }
+    /**
+     * @param mixed $md5_signature
+     */
+    public function setMd5Signature($md5_signature)
+    {
+        $this->md5_signature = $md5_signature;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMd5Signature()
+    {
+        return $this->md5_signature;
+    }
+
+    /**
+     * @param mixed $merchant_amount
+     */
+    public function setMerchantAmount($merchant_amount)
+    {
+        $this->merchant_amount = $merchant_amount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchantAmount()
+    {
+        return $this->merchant_amount;
+    }
+
+    /**
+     * @param mixed $merchant_currency
+     */
+    public function setMerchantCurrency($merchant_currency)
+    {
+        $this->merchant_currency = $merchant_currency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchantCurrency()
+    {
+        return $this->merchant_currency;
+    }
+
+    /**
+     * @param mixed $merchant_fields
+     */
+    public function setMerchantFields($merchant_fields)
+    {
+        $this->merchant_fields = $merchant_fields;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchantFields()
+    {
+        return $this->merchant_fields;
+    }
+
+    /**
+     * @param mixed $merchant_id
+     */
+    public function setMerchantId($merchant_id)
+    {
+        $this->merchant_id = $merchant_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchantId()
+    {
+        return $this->merchant_id;
+    }
+
+    /**
+     * @param mixed $merchant_transaction_id
+     */
+    public function setMerchantTransactionId($merchant_transaction_id)
+    {
+        $this->merchant_transaction_id = $merchant_transaction_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchantTransactionId()
+    {
+        return $this->merchant_transaction_id;
+    }
+
+    /**
+     * @param mixed $pay_from_email
+     */
+    public function setPayFromEmail($pay_from_email)
+    {
+        $this->pay_from_email = $pay_from_email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayFromEmail()
+    {
+        return $this->pay_from_email;
+    }
+
+    /**
+     * @param mixed $payed_amount
+     */
+    public function setPayedAmount($payed_amount)
+    {
+        $this->payed_amount = $payed_amount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayedAmount()
+    {
+        return $this->payed_amount;
+    }
+
+    /**
+     * @param mixed $payment_type
+     */
+    public function setPaymentType($payment_type)
+    {
+        $this->payment_type = $payment_type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPaymentType()
+    {
+        return $this->payment_type;
+    }
+
+    /**
+     * @param mixed $skrill_currency
+     */
+    public function setSkrillCurrency($skrill_currency)
+    {
+        $this->skrill_currency = $skrill_currency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSkrillCurrency()
+    {
+        return $this->skrill_currency;
+    }
+
+    /**
+     * @param mixed $skrill_transaction_id
+     */
+    public function setSkrillTransactionId($skrill_transaction_id)
+    {
+        $this->skrill_transaction_id = $skrill_transaction_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSkrillTransactionId()
+    {
+        return $this->skrill_transaction_id;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $customer_id
+     */
+    public function setCustomerId($customer_id)
+    {
+        $this->customer_id = $customer_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerId()
+    {
+        return $this->customer_id;
+    }
+
+
+    /**
+     * @param mixed $pay_to_email
+     */
+    public function setPayToEmail($pay_to_email)
+    {
+        $this->pay_to_email = $pay_to_email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayToEmail()
+    {
+        return $this->pay_to_email;
+    }
+}
