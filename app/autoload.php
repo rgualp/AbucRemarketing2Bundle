2baa083cb7296648e89c54b34126aa9a68ebffd8
Jakob <deadzac@gmx.net>
2013-10-25 11:18:56 +0200

    undo accidental deletions/overwrites and addition of temporary files

--- app/autoload.php
+++ app/autoload.php
@@ -4,13 +4,14 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+$loader->add('BeSimple',__DIR__.'/../vendor/bundles');
+
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
     $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
     $loader->add('Imagine',__DIR__.'/../vendor/imagine/lib');
-    $loader->add('BeSimple',__DIR__.'/../vendor/bundles');
     $loader->add('Avalanche',__DIR__.'/../vendor/bundles');
 }
 
