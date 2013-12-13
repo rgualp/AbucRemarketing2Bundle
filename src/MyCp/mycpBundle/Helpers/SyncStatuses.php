a70dc00ca20d19ea84b813a17c9fd12c6d0d79f4
Daniel <darthdaniel85@gmail.com>
2013-12-13 16:45:28 -0500

    adjusting sync systems

---
+++ src/MyCp/mycpBundle/Helpers/SyncStatuses.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Description of SyncStatuses
+ *
+ * @author Daniel
+ */
+
+namespace MyCp\mycpBundle\Helpers;
+
+class SyncStatuses {
+
+    const ADDED = "ADDED";
+    const UPDATED = "UPDATED";
+    const DELETED = "DELETED";
+    const SYNC = "SYNC";
+
+}
+
+?>
