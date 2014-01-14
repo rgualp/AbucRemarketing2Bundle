e47094c132caa8a1755a29a621b8b4d5d0e4f847
ErnestoABUC <desarrolloagil09@gmail.com>
2014-01-14 14:58:31 +0100

    Mapa en Voucher

--- src/MyCp/frontEndBundle/Controller/reservationController.php
+++ src/MyCp/frontEndBundle/Controller/reservationController.php
@@ -821,8 +821,9 @@ class reservationController extends Controller
         foreach($own_res as $own)
         {
             // load map from google static map
+            //$image = file_get_contents('http://maps.googleapis.com/maps/api/staticmap?center=23.13648,-82.365148&zoom=15&size=200x200&sensor=false');
             $image = file_get_contents('http://maps.googleapis.com/maps/api/staticmap?center=23.13648,-82.365148&zoom=15&size=200x200&sensor=false');
-            $fp  = fopen('bouchers/'.$id_booking.'_'.$own->getOwnResGenResId()->getGenResOwnId()->getOwnId().'.jpg', 'w+');
+            $fp  = fopen('bouchers/'.$id_booking.'_'.$own->getOwnResGenResId()->getGenResOwnId()->getOwnId().'.jpg', 'w');
             fputs($fp, $image);
             fclose($fp);
             unset($image);
