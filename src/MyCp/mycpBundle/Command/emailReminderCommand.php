bf3856c802c983c270fef2befc3a858531db1b7e
Jakob <deadzac@gmx.net>
2014-06-11 14:17:48 +0200

    Refactored reservation reminder email command for production

--- src/MyCp/mycpBundle/Command/emailReminderCommand.php
+++ src/MyCp/mycpBundle/Command/emailReminderCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
-
 class EmailReminderCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -18,50 +17,101 @@ class EmailReminderCommand extends ContainerAwareCommand
             ->setName('mycp_emails:reminder')
             ->setDefinition(array())
             ->setDescription('Send reminder mail reservation to users');
-
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Sending emails...');
         $container = $this->getContainer();
-        $em = $container->get('doctrine')->getEntityManager();
-        $gen_reservations =  $em->getRepository('mycpBundle:generalReservation')->get_reminder_available();
-        $array_photos=array();
-        $array_nigths=array();
-        $service_time=$container->get('time');
-        $service_email = $container->get('Email');
-        if($gen_reservations)
-            foreach($gen_reservations as $gen_reservation)
-            {
-                $reservations=$em->getRepository('mycpBundle:ownershipReservation')->findBy(array('own_res_gen_res_id'=>$gen_reservation->getGenResId()));
+        $em = $container->get('doctrine')->getManager();
+        $generalReservations = $em->getRepository('mycpBundle:generalReservation')->get_reminder_available();
 
-                foreach($reservations as $res)
-                {
-                    $photos=$em->getRepository('mycpBundle:ownership')->getPhotos($res->getOwnResGenResId()->getGenResOwnId()->getOwnId());
-                    array_push($array_photos,$photos);
-                    $array_dates= $service_time->dates_between($res->getOwnResReservationFromDate()->getTimestamp(),$res->getOwnResReservationToDate()->getTimestamp());
-                    array_push($array_nigths,count($array_dates));
+        $output->writeln('Starting reminder emails command...');
+
+        if(empty($generalReservations)) {
+            $output->writeln('No Reminder General Reservations found.');
+            return 0;
+        }
+
+        $output->writeln('Reminder General Reservations found: ' . count($generalReservations));
+
+        $arrayPhotos = array();
+        $arrayNights = array();
+        $timeService = $container->get('time');
+        $emailService = $container->get('Email');
+        $templatingService = $container->get('templating');
+        $translatorService = $container->get('translator');
+        $logger = $container->get('logger');
+
+        foreach($generalReservations as $generalReservation)
+        {
+            $generalReservationId = $generalReservation->getGenResId();
+
+            $reservations = $em
+                ->getRepository('mycpBundle:ownershipReservation')
+                ->findBy(array('own_res_gen_res_id' => $generalReservationId));
+
+            foreach($reservations as $res)
+            {
+                $photos = $em
+                    ->getRepository('mycpBundle:ownership')
+                    ->getPhotos(
+                        $res->getOwnResGenResId()->getGenResOwnId()->getOwnId()
+                    );
+
+                if(!empty($photos)) {
+                    array_push($arrayPhotos,$photos);
                 }
-                // Enviando mail al cliente
-                $user_tourist=$em->getRepository('mycpBundle:userTourist')->findOneBy(array('user_tourist_user'=>$gen_reservation->getGenResUserId()->getUserId()));
-                $locale = $this->get('translator');
-                $user_locale = strtolower($user_tourist->getUserTouristLanguage()->getLangCode());
-                $body = $this->render('frontEndBundle:mails:reminder_available.html.twig', array(
-                    'user'=>$gen_reservation->getGenResUserId(),
-                    'reservations'=>$reservations,
-                    'photos'=>$array_photos,
-                    'nights'=>$array_nigths,
-                    'user_locale' => $user_locale
-                ));
-                $subject = $locale->trans('REMINDER', array(), "messages", $user_locale);
-                $service_email->send_email(
-                    $subject, 'reservation@mycasaparticular.com', 'MyCasaParticular.com',
-                    $gen_reservation->getGenResUserId()->getUserEmail(),
-                    $body
-                );
+
+                $array_dates = $timeService
+                    ->dates_between(
+                        $res->getOwnResReservationFromDate()->getTimestamp(),
+                        $res->getOwnResReservationToDate()->getTimestamp()
+                    );
+                array_push($arrayNights, count($array_dates));
             }
 
+            // Enviando mail al cliente
+            $user_tourist = $em
+                ->getRepository('mycpBundle:userTourist')
+                ->findOneBy(array(
+                    'user_tourist_user' => $generalReservation->getGenResUserId()->getUserId()
+                ));
+
+            $userLocale = strtolower($user_tourist->getUserTouristLanguage()->getLangCode());
+            $translatorService->setLocale($userLocale);
+
+            $body = $templatingService
+                ->renderResponse('frontEndBundle:mails:reminder_available.html.twig', array(
+                    'user' => $generalReservation->getGenResUserId(),
+                    'reservations' => $reservations,
+                    'photos' => $arrayPhotos,
+                    'nights' => $arrayNights,
+                    'user_locale' => $userLocale
+                ));
+
+            $subject = $translatorService->trans('REMINDER', array(), "messages", $userLocale);
+            $emailAddress = $generalReservation->getGenResUserId()->getUserEmail();
+
+            $output->writeln('Try sending Reminder email to ' . $emailAddress
+                . ' for General Reservation ID ' . $generalReservationId);
+
+            try {
+                $emailService->send_email(
+                    $subject, 'reservation@mycasaparticular.com', 'MyCasaParticular.com',
+                    $emailAddress,
+                    $body
+                );
+
+                $output->writeln('Successfully sent Reminder email to ' . $emailAddress
+                    . ' for General Reservation ID ' . $generalReservationId);
+            } catch (\Exception $e) {
+                $message = "Could not send Email to address $emailAddress" . PHP_EOL . $e->getMessage();
+                $logger->warning($message);
+                $output->writeln($message);
+            }
+        }
+
         $output->writeln('Operation completed!!!');
+        return 0;
     }
 }
