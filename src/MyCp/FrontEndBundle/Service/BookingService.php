a2d321e8ad73973010fe4748b2e5d3460ec1b591
Jakob <deadzac@gmx.net>
2014-07-29 21:14:14 +0200

    Added payment post-processing to BookingService

--- src/MyCp/FrontEndBundle/Service/BookingService.php
+++ src/MyCp/FrontEndBundle/Service/BookingService.php
@@ -3,10 +3,19 @@
 namespace MyCp\FrontEndBundle\Service;
 
 use Doctrine\ORM\EntityManager;
+use MyCp\FrontEndBundle\Helpers\PaymentHelper;
+use MyCp\mycpBundle\Entity\booking;
+use MyCp\mycpBundle\Entity\payment;
+use MyCp\mycpBundle\Helpers\SyncStatuses;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BookingService extends Controller
 {
+    /**
+     * @var string
+     */
+    private $voucherDirectoryPath;
+
     /**
      * @var EntityManager
      */
@@ -18,10 +27,20 @@ class BookingService extends Controller
      */
     private $serviceChargeInCuc;
 
-    public function __construct(EntityManager $em, $serviceChargeInCuc)
+    public function __construct(EntityManager $em, $serviceChargeInCuc, $voucherDirectoryPath)
     {
         $this->em = $em;
         $this->serviceChargeInCuc = (float)$serviceChargeInCuc;
+
+        if (!is_dir($voucherDirectoryPath)) {
+            throw new \InvalidArgumentException('Invalid directory given: ' . $voucherDirectoryPath);
+        }
+
+        if (DIRECTORY_SEPARATOR != substr($voucherDirectoryPath, -1)) {
+            $voucherDirectoryPath .= DIRECTORY_SEPARATOR;
+        }
+
+        $this->voucherDirectoryPath = $voucherDirectoryPath;
     }
 
     /**
@@ -35,7 +54,7 @@ class BookingService extends Controller
         $timeService = $this->get('Time');
         $em = $this->em;
         $booking = $this->getBooking($bookingId);
-        $payment = $this->getPayment($booking);
+        $payment = $this->getPaymentByBooking($booking);
         $user = $this->getUserByBooking($booking);
 
         $currency = $payment->getCurrency();
@@ -167,6 +186,322 @@ class BookingService extends Controller
     }
 
     /**
+     * Executes the post-processing of a booking's payment.
+     *
+     * This includes sending out emails to the respective stakeholders
+     * and setting the statuses of corresponding reservations.
+     *
+     * @param payment $payment
+     * @throws \LogicException
+     */
+    public function postProcessBookingPayment(payment $payment)
+    {
+        $booking = $payment->getBooking();
+        $bookingId = $booking->getBookingId();
+        $status = $payment->getStatus();
+
+        if ($status !== PaymentHelper::STATUS_PENDING
+            && $status !== PaymentHelper::STATUS_SUCCESS) {
+            return;
+        }
+
+        $paymentPending = $status == PaymentHelper::STATUS_PENDING;
+        $this->updateReservationStatuses($bookingId, $status);
+        $this->processPaymentEmails($booking, $paymentPending);
+        $this->setPaymentStatusProcessed($payment);
+    }
+
+    /**
+     * Processes the payment emails by sending them out to
+     *  - the customer
+     *  - the reservation team
+     *  - the accommodation owner(s)
+     *
+     * @param booking $booking
+     * @param $paymentPending
+     */
+    private function processPaymentEmails(booking $booking, $paymentPending)
+    {
+        $bookingId = $booking->getBookingId();
+        $user = $this->getUserFromBooking($booking);
+        $userId = $user->getUserId();
+        $userTourist = $this->getUserTourist($userId, $bookingId);
+        $ownershipReservations = $this->getOwnershipReservations($bookingId);
+
+        $arrayPhotos = array();
+        $arrayNights = array();
+        $arrayNightsByOwnershipReservation = array();
+        $arrayHouses = array();
+        $arrayHousesIds = array();
+        $arrayOwnershipReservationByHouse = array();
+        $timeService = $this->get('time');
+
+        $cont = 0;
+
+        foreach ($ownershipReservations as $own) {
+            $rootOwn = $own->getOwnResGenResId()->getGenResOwnId();
+            $rootOwnId = $rootOwn->getOwnId();
+
+            $photos = $this->em
+                ->getRepository('mycpBundle:ownership')
+                ->getPhotos($rootOwnId);
+
+            array_push($arrayPhotos, $photos);
+            $array_dates = $timeService->dates_between(
+                $own->getOwnResReservationFromDate()->getTimestamp(),
+                $own->getOwnResReservationToDate()->getTimestamp()
+            );
+            array_push($arrayNights, count($array_dates) - 1);
+            $arrayNightsByOwnershipReservation[$own->getOwnResId()] = count($array_dates) - 1;
+
+            $insert = true;
+            foreach ($arrayHousesIds as $item) {
+                if ($rootOwnId == $item) {
+                    $insert = false;
+                }
+            }
+
+            if ($insert) {
+                array_push($arrayHousesIds, $rootOwnId);
+                array_push($arrayHouses, $rootOwn);
+            }
+
+            if (isset($arrayOwnershipReservationByHouse[$rootOwnId])) {
+                $temp_array = $arrayOwnershipReservationByHouse[$rootOwnId];
+            } else {
+                $temp_array = array();
+            }
+
+            array_push($temp_array, $own);
+            $arrayOwnershipReservationByHouse[$rootOwnId] = $temp_array;
+            $cont++;
+        }
+
+        $response = $this->getPrintableBookingConfirmationResponse($bookingId);
+
+        $pdfName = 'voucher' . $userId . '_' . $bookingId;
+        $pdfFilePath = $this->getVoucherPdfFilePath($pdfName);
+
+        $pdfService = $this->get('front_end.services.pdf');
+        $success = $pdfService->storeHtmlAsPdf($response, $pdfFilePath);
+
+        if (!$success) {
+            // PDF could not be stored, so ignore attachment for now
+            $pdfFilePath = null;
+        }
+
+        // Send email to customer
+        $emailService = $this->get('Email');
+
+        $userLocale = strtolower($userTourist->getUserTouristLanguage()->getLangCode());
+        $body = $this->render('FrontEndBundle:mails:email_offer_available.html.twig', array(
+            'booking' => $bookingId,
+            'user' => $user,
+            'reservations' => $ownershipReservations,
+            'photos' => $arrayPhotos,
+            'nights' => $arrayNights,
+            'user_locale' => $userLocale
+        ));
+
+        $locale = $this->get('translator');
+        $subject = $locale->trans('PAYMENT_CONFIRMATION', array(), "messages", $userLocale);
+
+        $logger = $this->get('logger');
+        $userEmail = trim($user->getUserEmail());
+
+        try {
+            $emailService->send_email(
+                $subject,
+                'reservation1@mycasaparticular.com',
+                $subject . ' - MyCasaParticular.com',
+                $userEmail,
+                $body,
+                $pdfFilePath
+            );
+
+            $logger->info('Successfully sent email to user ' . $userEmail . ', PDF path : ' .
+                (isset($pdfFilePath) ? $pdfFilePath : '<empty>'));
+        } catch (\Exception $e) {
+            $logger->error(sprintf(
+                    'EMAIL: Could not send Email to User. Booking ID: %s, Email: %s',
+                    $bookingId, $userEmail));
+            $logger->error($e->getMessage());
+        }
+
+        // send email to reservation team
+        foreach ($arrayOwnershipReservationByHouse as $owns) {
+            $bodyRes = $this->render(
+                'FrontEndBundle:mails:rt_payment_confirmation.html.twig',
+                array(
+                    'user' => $user,
+                    'user_tourist' => array($userTourist),
+                    'reservations' => $owns,
+                    'nights' => $arrayNightsByOwnershipReservation,
+                    'payment_pending' => $paymentPending
+                )
+            );
+
+            try {
+                $emailService->send_email(
+                    'Confirmación de pago',
+                    'no-reply@mycasaparticular.com',
+                    'MyCasaParticular.com',
+                    'reservation@mycasaparticular.com',
+                    $bodyRes
+                );
+
+                $logger->info('Successfully sent email to reservation team. Booking ID: ' . $bookingId);
+            } catch (\Exception $e) {
+                $logger->error('EMAIL: Could not send Email to reservation team. Booking ID: ' . $bookingId);
+                $logger->error($e->getMessage());
+            }
+        }
+
+        // send email to accommodation owner
+        foreach ($arrayOwnershipReservationByHouse as $owns) {
+            $bodyOwner = $this->render(
+                'FrontEndBundle:mails:email_house_confirmation.html.twig',
+                array(
+                    'user' => $user,
+                    'user_tourist' => array($userTourist),
+                    'reservations' => $owns,
+                    'nights' => $arrayNightsByOwnershipReservation
+                )
+            );
+
+            $ownerEmail = $owns[0]->getOwnResGenResId()->getGenResOwnId()->getOwnEmail1();
+            $ownerEmail = trim($ownerEmail);
+
+            if (empty($ownerEmail)) {
+                $logger->warning('EMAIL: Could not send Email to Casa Owner as the Email address is empty. Booking ID: ' .
+                    $bookingId . '. General Reservation ID: ' .
+                    $owns[0]->getOwnResGenResId()->getGenResId() . '.');
+            } else {
+                try {
+                    $emailService->send_email(
+                        'Confirmación de reserva',
+                        'no-reply@mycasaparticular.com',
+                        'MyCasaParticular.com',
+                        $ownerEmail,
+                        $bodyOwner
+                    );
+
+                    $logger->info('Successfully sent email to Casa Owner. Booking ID: ' .
+                        $bookingId . ', Email: ' . $ownerEmail);
+                } catch (\Exception $e) {
+                    $logger->error('EMAIL: Could not send Email to Casa Owner. Booking ID: ' .
+                        $bookingId . '. General Reservation ID: ' .
+                        $owns[0]->getOwnResGenResId()->getGenResId() . '. Email: ' . $ownerEmail);
+                    $logger->error($e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * Updates the statuses of the ownership and general reservations
+     * according to the corresponding payment status.
+     *
+     * @param $bookingId
+     * @param $paymentStatus
+     */
+    private function updateReservationStatuses($bookingId, $paymentStatus)
+    {
+        $ownershipReservations = $this->getOwnershipReservations($bookingId);
+
+        foreach ($ownershipReservations as $own) {
+            $own->setOwnResSyncSt(SyncStatuses::UPDATED);
+
+            if ($paymentStatus == PaymentHelper::STATUS_PENDING
+                || $paymentStatus == PaymentHelper::STATUS_SUCCESS) {
+                $generalReservation = $own->getOwnResGenResId();
+                $generalReservation->setGenResStatus(2); // TODO: What is status 2??? Create helper with the statuses
+                $own->setOwnResStatus(5); // TODO: What is status 5??? Create helper with the statuses
+                $this->em->persist($generalReservation);
+            }
+
+            $this->em->persist($own);
+        }
+
+        $this->em->flush();
+    }
+
+    /**
+     * Set the payment status to STATUS_PROCESSED
+     *
+     * @param payment $payment
+     */
+    private function setPaymentStatusProcessed(payment $payment)
+    {
+        $payment->setStatus(PaymentHelper::STATUS_PROCESSED);
+        $this->em->persist($payment);
+        $this->em->flush();
+    }
+
+    /**
+     * @param booking $booking
+     * @return null|object
+     * @throws \LogicException
+     */
+    private function getUserFromBooking(booking $booking)
+    {
+        $user = $this->em
+            ->getRepository('mycpBundle:user')
+            ->find($booking->getBookingUserId());
+
+        if (empty($user)) {
+            throw new \LogicException(
+                sprintf('User with ID %s not found (Booking ID %s)',
+                    $booking->getBookingUserId(), $booking->getBookingId()));
+        }
+        return $user;
+    }
+
+    /**
+     * @param $userId
+     * @param $bookingId
+     * @return null|object
+     * @throws \LogicException
+     */
+    private function getUserTourist($userId, $bookingId)
+    {
+        $userTourist = $this->em
+            ->getRepository('mycpBundle:userTourist')
+            ->findOneBy(array('user_tourist_user' => $userId));
+
+        if (empty($userTourist)) {
+            throw new \LogicException(
+                sprintf('UserTourist for user with ID %s not found (Booking ID %s)',
+                    $userId, $bookingId));
+        }
+        return $userTourist;
+    }
+
+    /**
+     * @param $bookingId
+     * @return array
+     */
+    private function getOwnershipReservations($bookingId)
+    {
+        $ownershipReservations = $this->em
+            ->getRepository('mycpBundle:ownershipReservation')
+            ->findBy(array('own_res_reservation_booking' => $bookingId));
+        return $ownershipReservations;
+    }
+
+    /**
+     * Returns the file path for a voucher.
+     *
+     * @param $pdfName
+     * @return string
+     */
+    private function getVoucherPdfFilePath($pdfName)
+    {
+        $pdfFilePath = $this->voucherDirectoryPath . "$pdfName.pdf";
+        return $pdfFilePath;
+    }
+
+    /**
      * @param $bookingId
      * @return mixed
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
@@ -208,7 +543,7 @@ class BookingService extends Controller
      * @return mixed
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
-    private function getPayment($booking)
+    private function getPaymentByBooking($booking)
     {
         $payment = $this->em
             ->getRepository('mycpBundle:payment')
@@ -220,4 +555,5 @@ class BookingService extends Controller
 
         return $payment;
     }
+
 }
