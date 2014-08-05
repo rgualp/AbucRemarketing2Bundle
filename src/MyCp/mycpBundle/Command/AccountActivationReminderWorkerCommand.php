db9894242562fb70eb60c58db26ae41073ec343c
Jakob <deadzac@gmx.net>
2014-08-05 17:38:56 +0200

    Make use of EmailManager service in AccountActivationReminderWorkerCommand

--- src/MyCp/mycpBundle/Command/AccountActivationReminderWorkerCommand.php
+++ src/MyCp/mycpBundle/Command/AccountActivationReminderWorkerCommand.php
@@ -4,23 +4,40 @@ namespace MyCp\mycpBundle\Command;
 
 use Abuc\RemarketingBundle\Job\PredefinedJobs;
 use Abuc\RemarketingBundle\JobData\UserIdentificationJobData;
-use Abuc\RemarketingBundle\Worker\EmailWorker;
+use Abuc\RemarketingBundle\Worker\Worker;
 use Abuc\RemarketingBundle\JobData\JobData;
 use MyCp\mycpBundle\Entity\user;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 
-class AccountActivationReminderWorkerCommand extends EmailWorker
+class AccountActivationReminderWorkerCommand extends Worker
 {
-    private $em;
-    private $emailService;
-    private $templatingService;
+    /**
+     * 'translator' service
+     * @var
+     */
     private $translatorService;
+
+    /**
+     * 'Secure' service
+     *
+     * @var
+     */
     private $securityService;
+
+    /**
+     * 'router' service
+     *
+     * @var
+     */
     private $router;
-    private $numRetries;
+
+    /**
+     * 'mycp.service.email_manager' service
+     *
+     * @var \MyCp\mycpBundle\Helpers\EmailManager
+     */
+    private $emailManager;
 
     /**
      * {@inheritDoc}
@@ -30,10 +47,6 @@ class AccountActivationReminderWorkerCommand extends EmailWorker
         $this
             ->setName('mycp:worker:signupreminder')
             ->setDefinition(array())
-            ->addOption('retries',
-                'r',
-                InputOption::VALUE_OPTIONAL,
-                'Number of retries for email sending. If not set retries=3 is set.')
             ->setDescription('Sends a reminder email after 24h if a user has not yet activated his account')
             ->setJobName(PredefinedJobs::JOB_SIGNUP_REMINDER);
     }
@@ -54,9 +67,8 @@ class AccountActivationReminderWorkerCommand extends EmailWorker
 
         $output->writeln('Processing Account Activation Reminder for User ID ' . $userId);
 
-        $user = $this->getUserById($userId);
-        $this->initializeLocale($user);
-        $this->initializeNumRetries($input);
+        $user = $this->emailManager->getUserById($userId);// $this->getUserById($userId);
+        $this->emailManager->setLocaleByUser($user);
 
         if (!$user->getUserEnabled()) {
             $output->writeln('Send Account Activation Reminder Email to User ID ' . $userId);
@@ -80,38 +92,13 @@ class AccountActivationReminderWorkerCommand extends EmailWorker
         $emailSubject = $this->translatorService->trans('EMAIL_ACCOUNT_REGISTERED_SUBJECT_REMINDER');
         $activationUrl = $this->getActivationUrl($user);
 
-        $emailBody = $this->templatingService
-            ->renderResponse(
-                'FrontEndBundle:mails:enableAccountReminder.html.twig',
-                array('enableUrl' => $activationUrl)
-            )
-            ->getContent();
+        $emailBody = $this->emailManager->getViewContent(
+            'FrontEndBundle:mails:enableAccountReminder.html.twig',
+            array('enableUrl' => $activationUrl)
+        );
 
         $output->writeln("Send email to $userEmail, subject '$emailSubject' for User ID $userId");
-
-        for ($numRetries = $this->numRetries; $numRetries > 0; $numRetries--) {
-            try {
-                $this->emailService->send_templated_email(
-                    $emailSubject,
-                    'noreply@mycasaparticular.com',
-                    $userEmail,
-                    $emailBody
-                );
-
-                $this->sendOutEmails();
-                break;
-            } catch (\Swift_TransportException $e) {
-                $message = 'Swift_TransportException on Email send-out: ' . PHP_EOL;
-                $message .= $e->getMessage() . PHP_EOL;
-                $message .= $e->getTraceAsString();
-
-                // retry on transport exceptions
-                $this->getLogger()->error($message);
-                $output->writeln($message);
-            }
-
-            sleep(2);
-        }
+        $this->emailManager->sendTemplatedEmail($userEmail, $emailSubject, $emailBody);
     }
 
     /**
@@ -119,86 +106,12 @@ class AccountActivationReminderWorkerCommand extends EmailWorker
      */
     private function initializeServices()
     {
-        if (null === $this->em) {
-            $this->em = $this->getService('doctrine.orm.entity_manager');
-            $this->timeService = $this->getService('time');
-            $this->emailService = $this->getService('Email');
-            $this->templatingService = $this->getService('templating');
+        if (null === $this->emailManager) {
+            $this->emailManager = $this->getService('mycp.service.email_manager');
             $this->translatorService = $this->getService('translator');
             $this->securityService = $this->getService('Secure');
             $this->router = $this->getService('router');
         }
-    }
-
-    /**
-     * Initializes the locale for the translator service.
-     *
-     * @param user $user
-     */
-    private function initializeLocale(user $user)
-    {
-        $userTourist = $this->getTouristByUser($user);
-        $userLocale = strtolower($userTourist->getUserTouristLanguage()->getLangCode());
-        $this->translatorService->setLocale($userLocale);
-    }
-
-    /**
-     * Initializes the number of email send-out retries.
-     *
-     * @param InputInterface $input
-     * @throws \InvalidArgumentException
-     */
-    private function initializeNumRetries(InputInterface $input)
-    {
-        $numRetries = $input->getOption('retries');
-
-        if (!$numRetries) {
-            $numRetries = 3;
-        }
-
-        if (!is_numeric($numRetries)) {
-            throw new \InvalidArgumentException('Argument "retries" is not an integer');
-        }
-
-        $this->numRetries = (int) $numRetries;
-    }
-
-    /**
-     * @param $userId
-     * @return mixed
-     * @throws \LogicException
-     */
-    private function getUserById($userId)
-    {
-        $user = $this->em
-            ->getRepository('mycpBundle:user')
-            ->find($userId);
-
-        if (empty($user)) {
-            throw new \LogicException('No user found for ID ' . $userId);
-        }
-
-        return $user;
-    }
-
-    /**
-     * @param $user
-     * @return mixed
-     * @throws \LogicException
-     */
-    private function getTouristByUser($user)
-    {
-        $userTourist = $this->em
-            ->getRepository('mycpBundle:userTourist')
-            ->findOneBy(array(
-                'user_tourist_user' => $user
-            ));
-
-        if (empty($userTourist)) {
-            throw new \LogicException('No userTourist found for User ID ' . $user->getUserId());
-        }
-
-        return $userTourist;
     }
 
     /**
@@ -207,8 +120,7 @@ class AccountActivationReminderWorkerCommand extends EmailWorker
      */
     private function getActivationUrl($user)
     {
-        $encodedString = $this->securityService
-            ->encode_string($user->getUserEmail() . '///' . $user->getUserId());
+        $encodedString = $this->securityService->getEncodedUserString($user);
         $enableUrl = $this->router->generate('frontend_enable_user', array('string' => $encodedString), true);
         return $enableUrl;
     }
