297b296f23bd580fc89f083932141a76a9fc4e4a
YANET <yanet.moralesr@gmail.com>
2014-08-18 14:53:29 +0200

    Resolved #40: Using always_remember_me parameter in remember me section of backend user provider. The value of key was replaced by %secret% and the old value was copy in parameter.yml secret.

--- src/MyCp/mycpBundle/Helpers/Secure.php
+++ src/MyCp/mycpBundle/Helpers/Secure.php
@@ -1,93 +1,84 @@
 <?php
+
 namespace MyCp\mycpBundle\Helpers;
+
 use MyCp\mycpBundle\Entity\log;
 use MyCp\mycpBundle\Entity\user;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\HttpFoundation\Request;
 
+class Secure {
 
-class Secure
-{
     private $em;
     private $container;
     private $security_context;
 
-    public function __construct($entity_manager, $container, $security_context)
-    {
+    public function __construct($entity_manager, $container, $security_context) {
         $this->em = $entity_manager;
         $this->container = $container;
         $this->security_context = $security_context;
     }
 
-    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
-    {
-        $user = $this->security_context->getToken()->getUser();
-        $role_id=$user->getUserSubrole()->getRoleId();
-        $role_permissions=$this->em->getRepository('mycpBundle:rolePermission')->findBy(array('rp_role'=>$role_id));
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event) {
+        $permissions = array();
+        if ($this->security_context->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $user = $this->security_context->getToken()->getUser();
+            $role_id = $user->getUserSubrole()->getRoleId();
+            $role_permissions = $this->em->getRepository('mycpBundle:rolePermission')->findBy(array('rp_role' => $role_id));
 
-        $permissions=array();
-        foreach($role_permissions as $permission)
-        {
-            array_push($permissions,$permission->getRpPermission()->getPermRoute());
 
+            foreach ($role_permissions as $permission) {
+                array_push($permissions, $permission->getRpPermission()->getPermRoute());
+            }
         }
-        $this->container->get('session')->set('permissions',$permissions);
+        $this->container->get('session')->set('permissions', $permissions);
     }
 
-    public function verify_access()
-    {
-        if($this->container->get('session')->get('permissions'))
-        {
-            $route=$this->container->get('request')->attributes->all();
+    public function verify_access() {
+        if ($this->container->get('session')->get('permissions')) {
+            $route = $this->container->get('request')->attributes->all();
 
-            $route=$route['_route'];
-            $access=false;
-            foreach($this->container->get('session')->get('permissions') as $permission)
-            {
-                if($route==$permission)
-                {
-                    $access=true;
+            $route = $route['_route'];
+            $access = false;
+            foreach ($this->container->get('session')->get('permissions') as $permission) {
+                if ($route == $permission) {
+                    $access = true;
                     break;
                 }
             }
 
-            if($access==false)
-            {
-                $templating=$this->container->get('templating');
+            if ($access == false) {
+                $templating = $this->container->get('templating');
                 echo $templating->render('mycpBundle:utils:access_denied.html.twig');
                 exit();
             }
-
         }
     }
 
-    public function getEncodedUserString(user $user)
-    {
+    public function getEncodedUserString(user $user) {
         return $this->encode_string($user->getUserEmail() . '///' . $user->getUserId());
     }
 
-    public function encode_string($string)
-    {
-        $key=$this->container->getParameter('encode.key');
+    public function encode_string($string) {
+        $key = $this->container->getParameter('encode.key');
         $result = '';
-        for($i=0; $i<strlen($string); $i++) {
+        for ($i = 0; $i < strlen($string); $i++) {
             $char = substr($string, $i, 1);
-            $keychar = substr($key, ($i % strlen($key))-1, 1);
-            $char = chr(ord($char)+ord($keychar));
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) + ord($keychar));
             $result.=$char;
         }
         return base64_encode($result);
     }
 
-    public function decode_string($string)
-    {
-        $key=$this->container->getParameter('encode.key');
+    public function decode_string($string) {
+        $key = $this->container->getParameter('encode.key');
         $result = '';
         $string = base64_decode($string);
-        for($i=0; $i<strlen($string); $i++) {
+        for ($i = 0; $i < strlen($string); $i++) {
             $char = substr($string, $i, 1);
-            $keychar = substr($key, ($i % strlen($key))-1, 1);
-            $char = chr(ord($char)-ord($keychar));
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) - ord($keychar));
             $result.=$char;
         }
         return $result;
