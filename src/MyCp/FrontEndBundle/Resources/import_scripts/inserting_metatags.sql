8fdcfbb334f9b5b8701ac870d80759ee1d81d18f
YANET <yanet.moralesr@gmail.com>
2014-08-25 17:07:17 +0200

    Script for insert metatags data

---
+++ src/MyCp/FrontEndBundle/Resources/import_scripts/inserting_metatags.sql
@@ -0,0 +1,24 @@
+﻿﻿/*
+Inserting default metatags
+Author: Yanet Morales Ramirez
+*/
+
+insert into `metatag` (meta_section, meta_title)
+VALUES (1, "General"),
+(2, "Portada"),
+(3, "Destinos"),
+(4, "Alojamientos"),
+(5, "MyCasaTrip"),
+(6, "Favoritos"),
+(7, "Cómo Funciona"),
+(8, "Últimos Alojamientos Añadidos"),
+(9, "Alojamientos Económicos"),
+(10, "Alojamientos Rango Medio"),
+(11, "Alojamientos Premium"),
+(12, "Ciudades Más Visitadas"),
+(13, "Quiénes Somos"),
+(14, "Contáctenos"),
+(15, "Preguntas Frecuentes"),
+(16, "Términos Legales"),
+(17, "Seguridad y Privacidad"),
+(18, "Mapa del Sitio");
