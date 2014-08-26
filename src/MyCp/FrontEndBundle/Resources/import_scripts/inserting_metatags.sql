36e2b60d1f95a6fc4fa1d43fceda69e36a7069f1
YANET <yanet.moralesr@gmail.com>
2014-08-26 13:04:22 +0200

    Updating script for insert metaTag

--- src/MyCp/FrontEndBundle/Resources/import_scripts/inserting_metatags.sql
+++ src/MyCp/FrontEndBundle/Resources/import_scripts/inserting_metatags.sql
@@ -3,22 +3,22 @@ Inserting default metatags
 Author: Yanet Morales Ramirez
 */
 
-insert into `metatag` (meta_section, meta_title)
-VALUES (1, "General"),
-(2, "Portada"),
-(3, "Destinos"),
-(4, "Alojamientos"),
-(5, "MyCasaTrip"),
-(6, "Favoritos"),
-(7, "Cómo Funciona"),
-(8, "Últimos Alojamientos Añadidos"),
-(9, "Alojamientos Económicos"),
-(10, "Alojamientos Rango Medio"),
-(11, "Alojamientos Premium"),
-(12, "Ciudades Más Visitadas"),
-(13, "Quiénes Somos"),
-(14, "Contáctenos"),
-(15, "Preguntas Frecuentes"),
-(16, "Términos Legales"),
-(17, "Seguridad y Privacidad"),
-(18, "Mapa del Sitio");
+insert into `metatag` (meta_section, meta_title, meta_parent)
+VALUES (1, "General",NULL),
+(2, "Portada", 1),
+(3, "Destinos", 1),
+(4, "Alojamientos", 1),
+(5, "MyCasaTrip", 1),
+(6, "Favoritos", 1),
+(7, "Cómo Funciona", 1),
+(8, "Últimos Alojamientos Añadidos", 4),
+(9, "Alojamientos Económicos", 4),
+(10, "Alojamientos Rango Medio", 4),
+(11, "Alojamientos Premium", 4),
+(12, "Ciudades Más Visitadas", 3),
+(13, "Quiénes Somos", 1),
+(14, "Contáctenos", 1),
+(15, "Preguntas Frecuentes", 1),
+(16, "Términos Legales", 1),
+(17, "Seguridad y Privacidad", 1),
+(18, "Mapa del Sitio", 1);
