b8d56601eb72da4f884a0cdf016ee9ae6107dbea
YANET <yanet.moralesr@gmail.com>
2014-09-08 16:34:31 +0200

    Backend: Insert municipality

--- src/MyCp/mycpBundle/Controller/BackendMunicipalityController.php
+++ src/MyCp/mycpBundle/Controller/BackendMunicipalityController.php
@@ -5,37 +5,60 @@ namespace MyCp\mycpBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
-use MyCp\mycpBundle\Entity\lang;
-use MyCp\mycpBundle\Entity\photo;
-use MyCp\mycpBundle\Entity\log;
-use MyCp\mycpBundle\Entity\langFlag;
-use MyCp\mycpBundle\Form\langType;
+use MyCp\mycpBundle\Entity\municipality;
+use MyCp\mycpBundle\Form\municipalityType;
 use MyCp\mycpBundle\Helpers\BackendModuleName;
 
+class BackendMunicipalityController extends Controller {
 
-
-class BackendMunicipalityController extends Controller
-{
-    public function listAction($items_per_page)
-    {
-        /*$service_security= $this->get('Secure');
-        $service_security->verifyAccess();*/
-        $page=1;
-        if(isset($_GET['page']))$page=$_GET['page'];
+    public function listAction($items_per_page) {
+        /* $service_security= $this->get('Secure');
+          $service_security->verifyAccess(); */
+        $page = 1;
+        if (isset($_GET['page']))
+            $page = $_GET['page'];
         $em = $this->getDoctrine()->getEntityManager();
         $paginator = $this->get('ideup.simple_paginator');
         $paginator->setItemsPerPage($items_per_page);
-        $municipalities=$paginator->paginate($em->getRepository('mycpBundle:municipality')->getAll())->getResult();
+        $municipalities = $paginator->paginate($em->getRepository('mycpBundle:municipality')->getAll())->getResult();
 
-        $service_log= $this->get('log');
-        $service_log->saveLog('Visit',BackendModuleName::MODULE_MUNICIPALITY);
+        $service_log = $this->get('log');
+        $service_log->saveLog('Visit', BackendModuleName::MODULE_MUNICIPALITY);
 
-        return $this->render('mycpBundle:municipality:list.html.twig',array(
-            'municipalities'=>$municipalities,
-            'items_per_page'=>$items_per_page,
-            'current_page'=>$page,
-            'total_items'=>$paginator->getTotalItems()
+        return $this->render('mycpBundle:municipality:list.html.twig', array(
+                    'municipalities' => $municipalities,
+                    'items_per_page' => $items_per_page,
+                    'current_page' => $page,
+                    'total_items' => $paginator->getTotalItems()
         ));
     }
+
+    public function newAction(Request $request) {
+        /* $service_security= $this->get('Secure');
+          $service_security->verifyAccess(); */
+        $em = $this->getDoctrine()->getEntityManager();
+        $municipality = new municipality();
+        $provinces = $em->getRepository('mycpBundle:province')->findAll();
+        $data['provinces'] = $provinces;
+        $form = $this->createForm(new municipalityType($data), $municipality);
+        if ($request->getMethod() == 'POST') {
+            $post_form = $request->get('mycp_mycpbundle_municipalitytype');
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $province = $em->getRepository('mycpBundle:province')->find($post_form['mun_prov_id']);
+                $municipality->setMunProvId($province);
+                $em->persist($municipality);
+                $em->flush();
+                $message = 'Municipio añadido satisfactoriamente.';
+                $this->get('session')->getFlashBag()->add('message_ok', $message);
+
+                $service_log = $this->get('log');
+                $service_log->saveLog('Create entity ' . $post_form['mun_name'], BackendModuleName::MODULE_MUNICIPALITY);
+
+                return $this->redirect($this->generateUrl('mycp_list_municipality'));
+            }
+        }
+        return $this->render('mycpBundle:municipality:new.html.twig', array('form' => $form->createView(), 'data' => $data));
+    }
+
 }
