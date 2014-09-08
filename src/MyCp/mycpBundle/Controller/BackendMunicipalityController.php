9e3cb1d4cee68ef66342d49c00983d46ee238fab
YANET <yanet.moralesr@gmail.com>
2014-09-08 16:50:51 +0200

    Backend: Edit municipality

--- src/MyCp/mycpBundle/Controller/BackendMunicipalityController.php
+++ src/MyCp/mycpBundle/Controller/BackendMunicipalityController.php
@@ -61,4 +61,33 @@ class BackendMunicipalityController extends Controller {
         return $this->render('mycpBundle:municipality:new.html.twig', array('form' => $form->createView(), 'data' => $data));
     }
 
+    public function editAction($id_municipality, Request $request) {
+        /* $service_security= $this->get('Secure');
+          $service_security->verifyAccess(); */
+        $em = $this->getDoctrine()->getEntityManager();
+        $municipality = $em->getRepository('mycpBundle:municipality')->find($id_municipality);
+        $municipality->setMunProvId($municipality->getMunProvId()->getProvId());
+        $data = array();
+        $data['provinces'] = $em->getRepository('mycpBundle:province')->findAll();
+        $form = $this->createForm(new municipalityType($data), $municipality);
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $post_form = $request->get('mycp_mycpbundle_municipalitytype');
+            if ($form->isValid()) {
+                $province = $em->getRepository('mycpBundle:province')->find($post_form['mun_prov_id']);
+                $municipality->setMunProvId($province);
+                $em->persist($municipality);
+                $em->flush();
+                $message = 'Municipio actualizado satisfactoriamente.';
+                $this->get('session')->getFlashBag()->add('message_ok', $message);
+
+                $service_log = $this->get('log');
+                $service_log->saveLog('Edit entity ' . $post_form['mun_name'], BackendModuleName::MODULE_MUNICIPALITY);
+
+                return $this->redirect($this->generateUrl('mycp_list_municipality'));
+            }
+        }
+        return $this->render('mycpBundle:municipality:new.html.twig', array('form' => $form->createView(), 'data' => $data, 'id_municipality' => $id_municipality, 'edit' => true));
+    }
+
 }
