35674b6f2798aa2e2e4fd930cb6a3d39933848f0
YANET <yanet.moralesr@gmail.com>
2014-09-14 18:16:08 +0200

    SQL Script: Creating an sql script for linking every accommodation in database with a destination located in the accommodation's municipality

---
+++ src/MyCp/FrontEndBundle/Resources/import_scripts/linking_accommodations_with_destination.sql
@@ -0,0 +1,9 @@
+﻿﻿/*
+Linking every accommodation in database with a destination located in the accommodation's municipality
+Author: Yanet Morales Ramirez
+*/
+
+update ownership o
+   set o.own_destination = (select min(d.des_id) from destinationlocation dl
+			    join destination d on dl.des_loc_des_id = d.des_id
+			    where dl.des_loc_mun_id = o.own_address_municipality);
