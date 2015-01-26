2bd3b4e21d70695931ee63b8751973f53eb03e26
Yanet Morales <yanet.moralesr@gmail.com>
2015-01-26 15:05:57 +0100

    SQL Script: Script for fixing dates in generalReservations

---
+++ src/MyCp/FrontEndBundle/Resources/import_scripts/2015-01-26.sql
@@ -0,0 +1,10 @@
+﻿﻿/*
+SQL Script done since 2015-01-26
+Author: Yanet Morales Ramirez
+*/
+
+/*Fixing dates in generalReservation*/
+update generalreservation gr
+set gr.gen_res_from_date = (SELECT MIN(ow.own_res_reservation_from_date) FROM ownershipreservation ow WHERE ow.own_res_gen_res_id =  gr.gen_res_id ),
+		gr.gen_res_to_date = (SELECT MAX(ow1.own_res_reservation_to_date) FROM ownershipreservation ow1 WHERE ow1.own_res_gen_res_id =  gr.gen_res_id );
+
