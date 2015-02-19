0003d47626b98609209be35432d09e5535e463ca
Yanet Morales <yanet.moralesr@gmail.com>
2015-02-19 23:26:59 +0100

    Command: Adding column with accommodation's name in the email send weekly with new accommodations

--- src/MyCp/mycpBundle/Entity/ownershipRepository.php
+++ src/MyCp/mycpBundle/Entity/ownershipRepository.php
@@ -778,6 +778,7 @@ class ownershipRepository extends EntityRepository {
         $em = $this->getEntityManager();
         $query_string = "SELECT o.own_id as ownId,
                          o.own_mcp_code as mycpCode,
+                         o.own_name as name,
                          o.own_rooms_total as totalRooms,
                          o.own_homeowner_1 as owner1,
                          o.own_homeowner_2 as owner2,
