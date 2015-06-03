3d629526cda9a1612a86f73c13e0bc7e8ca21897
Yanet Morales <yanet.moralesr@gmail.com>
2015-06-03 13:33:20 -0400

    Adding condition if table exists before creating new table.

--- app/migrations/Version20150520192813.php
+++ app/migrations/Version20150520192813.php
@@ -18,9 +18,12 @@ class Version20150520192813 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE clientComment (comment_id INT AUTO_INCREMENT NOT NULL, comment_client_user INT DEFAULT NULL, comment_staff_user INT DEFAULT NULL, comment_text LONGTEXT NOT NULL, comment_date DATETIME NOT NULL, INDEX IDX_F75CC7C067E3D469 (comment_client_user), INDEX IDX_F75CC7C0ED442AE6 (comment_staff_user), PRIMARY KEY(comment_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE clientComment ADD CONSTRAINT FK_F75CC7C067E3D469 FOREIGN KEY (comment_client_user) REFERENCES user (user_id)');
-        $this->addSql('ALTER TABLE clientComment ADD CONSTRAINT FK_F75CC7C0ED442AE6 FOREIGN KEY (comment_staff_user) REFERENCES user (user_id)');
+        if(!$this->connection->getSchemaManager()->tablesExist(array('clientComment'))) {
+            $this->addSql('CREATE TABLE clientComment (comment_id INT AUTO_INCREMENT NOT NULL, comment_client_user INT DEFAULT NULL, comment_staff_user INT DEFAULT NULL, comment_text LONGTEXT NOT NULL, comment_date DATETIME NOT NULL, INDEX IDX_F75CC7C067E3D469 (comment_client_user), INDEX IDX_F75CC7C0ED442AE6 (comment_staff_user), PRIMARY KEY(comment_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+
+            $this->addSql('ALTER TABLE clientComment ADD CONSTRAINT FK_F75CC7C067E3D469 FOREIGN KEY (comment_client_user) REFERENCES user (user_id)');
+            $this->addSql('ALTER TABLE clientComment ADD CONSTRAINT FK_F75CC7C0ED442AE6 FOREIGN KEY (comment_staff_user) REFERENCES user (user_id)');
+        }
     }
 
     /**
