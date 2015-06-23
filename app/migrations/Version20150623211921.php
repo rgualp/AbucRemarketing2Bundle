bb8f962cdfc70fde3e8f465331462c06867581c5
Luis Ernesto <nes9ti@gmail.com>
2015-06-23 15:31:33 -0400

    added general ownership report views!

---
+++ app/migrations/Version20150623211921.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150623211921 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {   $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("set @nomParentId = (SELECT (nom_id) FROM nomenclatorstat WHERE nom_name LIKE 'Resúmen')");
+        $this->addSql("INSERT INTO nomenclatorstat (nom_parent, nom_name) VALUES (@nomParentId, 'Total')");
+
+    }
+
+    public function down(Schema $schema)
+    {  $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+       $this->addSql("set @nomParentId = (SELECT (nom_id) FROM nomenclatorstat WHERE nom_name LIKE 'Resúmen')");
+       $this->addSql("DELETE FROM nomenclatorstat WHERE nom_name = 'Total' AND nom_parent= @nomParentId");
+
+    }
+}
