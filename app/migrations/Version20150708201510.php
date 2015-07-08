7229b6295cd23b09af1d84311516a7b520dae052
Yanet Morales <yanet.moralesr@gmail.com>
2015-07-08 14:21:34 -0400

    Deployment: Testing if migrations works in develop script

---
+++ app/migrations/Version20150708201510.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150708201510 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("INSERT INTO nomenclatorstat (nom_parent, nom_name) VALUES (NULL, 'Reservas')");
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("DELETE FROM nomenclatorstat WHERE nom_name LIKE 'Reservas'");
+    }
+}
