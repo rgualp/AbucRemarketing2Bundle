2bfc87427723149bf4b3d2a285e67e72c819790c
Yanet Morales <yanet.moralesr@gmail.com>
2015-07-20 16:56:48 -0400

    MYC-133: Creating repository for Accommodation-Reservation report

---
+++ app/migrations/Version20150720224726.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150720224726 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE ownershipreservationstat (stat_id INT AUTO_INCREMENT NOT NULL, stat_accommodation INT DEFAULT NULL, stat_nomenclator INT DEFAULT NULL, stat_date DATE NOT NULL, stat_value VARCHAR(255) NOT NULL, INDEX IDX_E35D06D71B93A1F9 (stat_accommodation), INDEX IDX_E35D06D7210B833C (stat_nomenclator), PRIMARY KEY(stat_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE ownershipreservationstat ADD CONSTRAINT FK_E35D06D71B93A1F9 FOREIGN KEY (stat_accommodation) REFERENCES ownership (own_id)');
+        $this->addSql('ALTER TABLE ownershipreservationstat ADD CONSTRAINT FK_E35D06D7210B833C FOREIGN KEY (stat_nomenclator) REFERENCES nomenclatorstat (nom_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE ownershipreservationstat');
+    }
+}
---
+++ src/MyCp/mycpBundle/Entity/ownershipReservationStat.php
@@ -0,0 +1,155 @@
+<?php
+
+
+namespace MyCp\mycpBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ownershipStat
+ *
+ * @ORM\Table(name="ownershipreservationstat")
+ * @ORM\Entity(repositoryClass="MyCp\mycpBundle\Entity\ownershipReservationStatRepository")
+ *
+ */
+class ownershipReservationStat
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="stat_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $stat_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ownership")
+     * @ORM\JoinColumn(name="stat_accommodation",referencedColumnName="own_id")
+     */
+    private $stat_accommodation;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="nomenclatorStat")
+     * @ORM\JoinColumn(name="stat_nomenclator",referencedColumnName="nom_id", nullable=true)
+     */
+    private $stat_nomenclator;
+
+    /**
+     * @var date
+     *
+     * @ORM\Column(name="stat_date", type="date")
+     */
+    private $stat_date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stat_value", type="string")
+     */
+    private $stat_value;
+
+
+    /**
+     * Get stat_id
+     *
+     * @return integer
+     */
+    public function getStatId()
+    {
+        return $this->stat_id;
+    }
+
+    /**
+     * Set stat_accommodation
+     *
+     * @param ownership $accommodation
+     * @return $this
+     */
+    public function setStatAccommodation(ownership $accommodation)
+    {
+        $this->stat_accommodation = $accommodation;
+
+        return $this;
+    }
+
+    /**
+     * Get stat_accommodation
+     *
+     * @return ownership
+     */
+    public function getStatAccommodation()
+    {
+        return $this->stat_accommodation;
+    }
+
+    /**
+     * Set stat_nomenclator
+     *
+     * @param nomenclatorStat $nomenclatorStat
+     * @return $this
+     */
+    public function setStatNomenclator(nomenclatorStat $nomenclatorStat)
+    {
+        $this->stat_nomenclator = $nomenclatorStat;
+
+        return $this;
+    }
+
+    /**
+     * Get stat_nomenclator
+     *
+     * @return nomenclatorStat
+     */
+    public function getStatNomenclator()
+    {
+        return $this->stat_nomenclator;
+    }
+
+    /**
+     * Get stat_value
+     *
+     * @return string
+     */
+    public function getStatValue()
+    {
+        return $this->stat_value;
+    }
+
+    /**
+     * Set stat_value
+     * @param $value
+     * @return $this
+     */
+    public function setStatValue($value)
+    {
+        $this->stat_value = $value;
+        return $this;
+    }
+
+    /**
+     * Get stat_date
+     *
+     * @return string
+     */
+    public function getStatDate()
+    {
+        return $this->stat_date;
+    }
+
+    /**
+     * Set stat_date
+     * @param $value
+     * @return $this
+     */
+    public function setStatDate($value)
+    {
+        $this->stat_date = $value;
+        return $this;
+    }
+
+    public function __toString(){
+        return $this->stat_value;
+    }
+
+}
