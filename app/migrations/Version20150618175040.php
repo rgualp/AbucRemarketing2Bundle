0063eb1ae0f1e80b72c0096f170cb5af2ede508e
Yanet Morales <yanet.moralesr@gmail.com>
2015-07-24 16:31:42 -0400

    Revision v1.11: Adding conditions to migrations files

--- app/migrations/Version20150618175040.php
+++ app/migrations/Version20150618175040.php
@@ -18,9 +18,11 @@ class Version20150618175040 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE ownershipstat (stat_id INT AUTO_INCREMENT NOT NULL, stat_municipality INT DEFAULT NULL, stat_nomenclator INT DEFAULT NULL, stat_value VARCHAR(255) NOT NULL, INDEX IDX_707D1D646E4A7B82 (stat_municipality), INDEX IDX_707D1D64210B833C (stat_nomenclator), PRIMARY KEY(stat_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE ownershipstat ADD CONSTRAINT FK_707D1D646E4A7B82 FOREIGN KEY (stat_municipality) REFERENCES municipality (mun_id)');
-        $this->addSql('ALTER TABLE ownershipstat ADD CONSTRAINT FK_707D1D64210B833C FOREIGN KEY (stat_nomenclator) REFERENCES nomenclatorstat (nom_id)');
+        if (!$this->connection->getSchemaManager()->tablesExist(array('ownershipstat'))) {
+            $this->addSql('CREATE TABLE ownershipstat (stat_id INT AUTO_INCREMENT NOT NULL, stat_municipality INT DEFAULT NULL, stat_nomenclator INT DEFAULT NULL, stat_value VARCHAR(255) NOT NULL, INDEX IDX_707D1D646E4A7B82 (stat_municipality), INDEX IDX_707D1D64210B833C (stat_nomenclator), PRIMARY KEY(stat_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+            $this->addSql('ALTER TABLE ownershipstat ADD CONSTRAINT FK_707D1D646E4A7B82 FOREIGN KEY (stat_municipality) REFERENCES municipality (mun_id)');
+            $this->addSql('ALTER TABLE ownershipstat ADD CONSTRAINT FK_707D1D64210B833C FOREIGN KEY (stat_nomenclator) REFERENCES nomenclatorstat (nom_id)');
+        }
     }
 
     /**
