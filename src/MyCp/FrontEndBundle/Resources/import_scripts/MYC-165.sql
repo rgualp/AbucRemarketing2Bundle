2c7853e79b02f1ea256319895a9f0b9e1af8e509
Yanet Morales <yanet.moralesr@gmail.com>
2015-09-01 19:00:39 -0400

    MYC-165 #resolve

--- src/MyCp/FrontEndBundle/Resources/import_scripts/MYC-165.sql
+++ src/MyCp/FrontEndBundle/Resources/import_scripts/MYC-165.sql
@@ -3,9 +3,11 @@ List of accommodation's with emails
 Author: Yanet Morales Ramirez
 */
 
-select o.own_mcp_code as CODIGO, o.own_name as CASA, o.own_email_1 as CORREO1, o.own_email_2 as CORREO2, p.prov_phone_code as CODIGO_TELEFONICO, o.own_phone_number as TELEFONO, o.own_mobile_number as MOVIL,
+select o.own_mcp_code as CODIGO, o.own_name as CASA, o.own_email_1 as CORREO1, o.own_email_2 as CORREO2,
+       o.own_homeowner_1 as PROPIETARIO, o.own_homeowner_2 as PROPIETARIO2, p.prov_phone_code as CODIGO_TELEFONICO, o.own_phone_number as TELEFONO, o.own_mobile_number as MOVIL,
        o.own_address_street as CALLE, o.own_address_number as NUMERO, o.own_address_between_street_1 as ENTRE1, o.own_address_between_street_2 as ENTRE2,
-       m.mun_name as MUNICIPIO, p.prov_name as PROVINCIA
+       m.mun_name as MUNICIPIO, p.prov_name as PROVINCIA,
+       o.own_saler as GESTOR
 from ownership o
   join province p on o.own_address_province = p.prov_id
   join municipality m on m.mun_id = o.own_address_municipality
