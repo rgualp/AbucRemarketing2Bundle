92e78b7ebe3853388993ab9f2f29cc1eb9317609
Yanet Morales <yanet.moralesr@gmail.com>
2015-09-01 18:47:02 -0400

    MYC-165 #comment Creating query for data

---
+++ src/MyCp/FrontEndBundle/Resources/import_scripts/MYC-165.sql
@@ -0,0 +1,13 @@
+﻿/*
+List of accommodation's with emails
+Author: Yanet Morales Ramirez
+*/
+
+select o.own_mcp_code as CODIGO, o.own_name as CASA, o.own_email_1 as CORREO1, o.own_email_2 as CORREO2, p.prov_phone_code as CODIGO_TELEFONICO, o.own_phone_number as TELEFONO, o.own_mobile_number as MOVIL,
+       o.own_address_street as CALLE, o.own_address_number as NUMERO, o.own_address_between_street_1 as ENTRE1, o.own_address_between_street_2 as ENTRE2,
+       m.mun_name as MUNICIPIO, p.prov_name as PROVINCIA
+from ownership o
+  join province p on o.own_address_province = p.prov_id
+  join municipality m on m.mun_id = o.own_address_municipality
+where o.own_status = 1 and (o.own_email_1 != "" or o.own_email_2 != "")
+order by own_address_province, own_mcp_code
