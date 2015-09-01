649cb7d6f9d4235b4bb9e77a68150e7ea9c0798e
Yanet Morales <yanet.moralesr@gmail.com>
2015-09-01 14:39:18 -0400

    Version v2.0.0

--- app/migrations/Version20150720224726.php
+++ app/migrations/Version20150720224726.php
@@ -19,7 +19,7 @@ class Version20150720224726 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         if (!$this->connection->getSchemaManager()->tablesExist(array('ownershipreservationstat'))) {
-            $this->addSql('CREATE TABLE ownershipreservationstat (stat_id INT AUTO_INCREMENT NOT NULL, stat_accommodation INT DEFAULT NULL, stat_nomenclator INT DEFAULT NULL, stat_date DATE NOT NULL, stat_value VARCHAR(255) NOT NULL, INDEX IDX_E35D06D71B93A1F9 (stat_accommodation), INDEX IDX_E35D06D7210B833C (stat_nomenclator), PRIMARY KEY(stat_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+            $this->addSql('CREATE TABLE ownershipreservationstat (stat_id INT AUTO_INCREMENT NOT NULL, stat_accommodation INT DEFAULT NULL, stat_nomenclator INT DEFAULT NULL, stat_date_from DATE NOT NULL, stat_date_to DATE NOT NULL, stat_value VARCHAR(255) NOT NULL, INDEX IDX_E35D06D71B93A1F9 (stat_accommodation), INDEX IDX_E35D06D7210B833C (stat_nomenclator), PRIMARY KEY(stat_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
             $this->addSql('ALTER TABLE ownershipreservationstat ADD CONSTRAINT FK_E35D06D71B93A1F9 FOREIGN KEY (stat_accommodation) REFERENCES ownership (own_id)');
             $this->addSql('ALTER TABLE ownershipreservationstat ADD CONSTRAINT FK_E35D06D7210B833C FOREIGN KEY (stat_nomenclator) REFERENCES nomenclatorstat (nom_id)');
         }
