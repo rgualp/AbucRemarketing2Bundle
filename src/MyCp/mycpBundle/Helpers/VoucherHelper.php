f43bad8e7d968eb9ca21ff879933bc49fd566ec2
Yanet Morales <yanet.moralesr@gmail.com>
2015-10-29 16:22:04 -0400

    New voucher. Changing voucher template. Testing functions

--- src/MyCp/mycpBundle/Helpers/VoucherHelper.php
+++ src/MyCp/mycpBundle/Helpers/VoucherHelper.php
@@ -62,7 +62,7 @@ class VoucherHelper {
                 else
                     $pdfFilePath = $bookingService->createBookingVoucherIfNotExisting($bookId);
 
-                $ownershipReservations = $entity_manager->getRepository('mycpBundle:ownershipReservation')
+                /*$ownershipReservations = $entity_manager->getRepository('mycpBundle:ownershipReservation')
                         ->findBy(array('own_res_reservation_booking' => $bookId));
                 $serviceTime = $controller->get('time');
                 $nights = array();
@@ -70,21 +70,73 @@ class VoucherHelper {
                 foreach ($ownershipReservations as $res) {
                     $resNights = $serviceTime->nights($res->getOwnResReservationFromDate()->getTimestamp(), $res->getOwnResReservationToDate()->getTimestamp());
                     array_push($nights, $resNights);
-                }
+                }*/
 
                 $userLocale = strtolower($userTourist->getUserTouristLanguage()->getLangCode());
-                $body = $controller->render('FrontEndBundle:mails:sendVoucher.html.twig', array(
+                $body = $controller->render('FrontEndBundle:mails:boletin.html.twig', array(
                     'user_locale' => $userLocale,
                     'user' => $user,
-                    'reservations' => $ownershipReservations,
+                    //'reservations' => $ownershipReservations,
                     'message' => $message,
-                    'nights' => $nights
+                    //'nights' => $nights
                 ));
 
                 $locale = $controller->get('translator');
                 $subject = $locale->trans($subjectTranslatedKey, array(), "messages", $userLocale);
                 $emailService->sendEmail(
                         $user->getUserEmail(), $subject, $body, 'no-reply@mycasaparticular.com', $pdfFilePath
+                );
+            }
+
+            $message = 'Se ha enviado satisfactoriamente al cliente el voucher asociado a la reservación ' . $genRes->getCASId();
+            $controller->get('session')->getFlashBag()->add('message_ok', $message);
+        } catch (\Exception $e) {
+            $CASId = \MyCp\FrontEndBundle\Helpers\ReservationHelper::getCASId($idReservation);
+            $message = 'Error al enviar el voucher asociado a la reservación ' . $CASId . ". " . $e->getMessage();
+            $controller->get('session')->getFlashBag()->add('message_error_main', $message);
+        }
+    }
+
+    public static function sendVoucherToClientTest($entity_manager, $bookingService, $emailService, $controller, $genRes, $subjectTranslatedKey, $email, $message = null, $createVoucher = true) {
+
+        $idReservation = $genRes->getGenResId();
+        try {
+            $bookings_ids = $entity_manager->getRepository('mycpBundle:generalReservation')->getBookings($idReservation);
+            $user = $genRes->getGenResUserId();
+            $userTourist = $entity_manager->getRepository('mycpBundle:userTourist')->findOneBy(array('user_tourist_user' => $user));
+
+            foreach ($bookings_ids as $bookId) {
+                $bookId = $bookId['booking_id'];
+
+                $pdfFilePath = "";
+                if($createVoucher)
+                    $pdfFilePath = $bookingService->createBookingVoucher($bookId);
+                else
+                    $pdfFilePath = $bookingService->createBookingVoucherIfNotExisting($bookId);
+
+                /*$ownershipReservations = $entity_manager->getRepository('mycpBundle:ownershipReservation')
+                        ->findBy(array('own_res_reservation_booking' => $bookId));
+                $serviceTime = $controller->get('time');
+                $nights = array();
+
+                foreach ($ownershipReservations as $res) {
+                    $resNights = $serviceTime->nights($res->getOwnResReservationFromDate()->getTimestamp(), $res->getOwnResReservationToDate()->getTimestamp());
+                    array_push($nights, $resNights);
+                }*/
+
+                $userLocale = strtolower($userTourist->getUserTouristLanguage()->getLangCode());
+                $body = $controller->render('FrontEndBundle:mails:boletin.html.twig', array(
+                    'user_locale' => $userLocale,
+                    'user' => $user,
+                    //'reservations' => $ownershipReservations,
+                    'message' => $message,
+                    //'nights' => $nights
+                ));
+
+                $locale = $controller->get('translator');
+                $subject = $locale->trans($subjectTranslatedKey, array(), "messages", $userLocale);
+                $emailService->sendEmail(
+                    $email, $subject, $body, 'no-reply@mycasaparticular.com', $pdfFilePath
                 );
             }
 
