b97a639e21b1a42d9977e98162d2a9dabe798501
Yanet Morales <yanet.moralesr@gmail.com>
2015-11-06 15:34:52 -0500

    MYC-274 #resolved

--- src/MyCp/mycpBundle/Resources/public/js/custom.js
+++ src/MyCp/mycpBundle/Resources/public/js/custom.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+    /*$('.accordion').on('shown', function () {
+        $(".icon-chevron-right").removeClass("icon-chevron-right").addClass("icon-chevron-down");
+    });
+
+    $('.accordion').on('hidden', function () {
+        $(".icon-chevron-down").removeClass("icon-chevron-down").addClass("icon-chevron-right");
+    });*/
+
+    /*$('.accordion').on('shown', function(){
+        $(this).parent().find(".icon-chevron-righ").removeClass("icon-chevron-righ").addClass("icon-chevron-down");
+    });
+    $('.accordion').on('hidden', function (){
+        $(this).parent().find(".icon-chevron-down").removeClass("icon-chevron-down").addClass("icon-chevron-righ");
+    });*/
+});
+
 $(function () {
     $('.tooltip_msg').tooltip();
 });
