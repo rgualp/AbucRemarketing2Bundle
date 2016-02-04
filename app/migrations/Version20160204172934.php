996ccfdef42115bf23272508c3af8309c3bfb0ab
Luis Ernesto Acosta Ortiz <nes9ti@gmail.com>
2016-02-04 11:33:23 -0500

    set migration query

---
+++ app/migrations/Version20160204172934.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160204172934 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('UPDATE user SET  user_enabled=1 WHERE user_role LIKE "%ROLE_CLIENT_STAFF%" AND user_enabled is NULL;');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
