b7d0dcfd41b60b049459037db73a6314ffa08f7a
Yanet Morales <yanet.moralesr@gmail.com>
2016-02-19 16:32:21 -0500

    award entity

--- src/MyCp/mycpBundle/Entity/award.php
+++ src/MyCp/mycpBundle/Entity/award.php
@@ -8,12 +8,30 @@ use Doctrine\ORM\Mapping\OneToMany;
 use MyCp\mycpBundle\Helpers\SyncStatuses;
 
 /**
- * room
+ * award
  *
  * @ORM\Table()
  * @ORM\Entity
  */
 class award {
 
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
     
+
 }
