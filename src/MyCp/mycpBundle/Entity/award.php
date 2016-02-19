ce39a6e0ae167fdbdacc4c2c7281fb57218c8297
Yanet Morales <yanet.moralesr@gmail.com>
2016-02-19 16:23:42 -0500

    Award entity

---
+++ src/MyCp/mycpBundle/Entity/award.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MyCp\mycpBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
+use MyCp\mycpBundle\Helpers\SyncStatuses;
+
+/**
+ * room
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class award {
+
+    
+}
