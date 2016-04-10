4ab3fe55226f14ef3a886118ea40cb8772059205
Yanet Morales <yanet.moralesr@gmail.com>
2016-04-10 09:58:36 -0400

    Adding maintenance page

---
+++ web/app_maintenance.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>MyCasaParticular</title>
+</head>
+<body>
+<img src="images/logo.jpg"><br/>
+<h3>Estamos en mantenimiento</h3>
+<h3>We are working here!</h3>
+</body>
+</html>
