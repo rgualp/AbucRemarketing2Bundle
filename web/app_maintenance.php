e98b74697b0ec81d6ba5ab7fa6b49834ac4568cb
Yanet Morales <yanet.moralesr@gmail.com>
2016-04-10 10:55:08 -0400

    Adding maintenance page

--- web/app_maintenance.php
+++ web/app_maintenance.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head lang="en">
-    <meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8" />
     <title>MyCasaParticular</title>
+    <link href="bundles/frontend/css/custom.css" rel="stylesheet" media="screen">
+    <link href="bundles/frontend/css/error_page.css" rel="stylesheet" media="screen">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
 </head>
 <body>
-<img src="images/logo.jpg"><br/>
-<h3>Estamos en mantenimiento</h3>
-<h3>We are working here!</h3>
+<div class="container" style="max-width: 450px">
+    <div>
+        <img src="bundles/frontend/img/mycp.png"/>
+    </div>
+
+    <div class="info_message banner">
+        <div style="font-size: 60px">Oops!</div>
+        <div class="message" style="line-height: 30px;">We are in maintenance</div>
+    </div>
+</div>
 </body>
 </html>
