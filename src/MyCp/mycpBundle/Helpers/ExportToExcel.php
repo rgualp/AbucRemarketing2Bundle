b649e91792219faeadb91bc31ac89c3b973decf9
Yanet Morales <yanet.moralesr@gmail.com>
2016-04-13 15:52:39 -0400

    Checking Excel fixed

--- src/MyCp/mycpBundle/Helpers/ExportToExcel.php
+++ src/MyCp/mycpBundle/Helpers/ExportToExcel.php
@@ -392,55 +392,46 @@ class ExportToExcel extends Controller {
 
         $checkins = $this->em->getRepository("mycpBundle:generalReservation")->getCheckins($date, $sort_by);
 
-        $total_nights = array();
-        $service_time = $this->container->get('time');
-        foreach ($checkins as $res) {
-            $genRes = $this->em->getRepository('mycpBundle:generalReservation')->find($res[0]['gen_res_id']);
-            $reservations = $this->em->getRepository('mycpBundle:ownershipReservation')->findBy(array("own_res_gen_res_id" => $res[0]['gen_res_id']), array("own_res_reservation_from_date" => "ASC"));
-            $nights = $genRes->getTotalStayedNights($reservations, $service_time);
-
-            $total_nights[$res[0]["gen_res_id"]] = $nights;
-        }
-
         foreach ($checkins as $check) {
             $data = array();
 
-            $data[0] = \MyCp\FrontEndBundle\Helpers\ReservationHelper::getCASId($check[0]["gen_res_id"]);
-            $resDate = $check[0]["gen_res_date"];
+            $data[0] = \MyCp\FrontEndBundle\Helpers\ReservationHelper::getCASId($check["gen_res_id"]);
+            $resDate = $check["gen_res_date"];
             $data[1] = $resDate->format("d/m/Y");
-            $data[2] = $check[0]["gen_res_own_id"]["own_mcp_code"];
-            $data[3] = $check[0]["gen_res_own_id"]["own_homeowner_1"];
-            if ($check[0]["gen_res_own_id"]["own_homeowner_2"] != "")
-                $data[3] .= " / " . $check[0]["gen_res_own_id"]["own_homeowner_2"];
+            $data[2] = $check["own_mcp_code"];
+            $data[3] = $check["own_homeowner_1"];
+            if ($check["own_homeowner_2"] != "")
+                $data[3] .= " / " . $check["own_homeowner_2"];
 
             $data[4] = "";
-            if ($check[0]["gen_res_own_id"]["own_phone_number"] != "")
-                $data[4] .= "(+53) " . $check[0]["gen_res_own_id"]["own_address_province"]["prov_phone_code"] . " " . $check[0]["gen_res_own_id"]["own_phone_number"];
+            if ($check["own_phone_number"] != "")
+                $data[4] .= "(+53) " . $check["prov_phone_code"] . " " . $check["own_phone_number"];
 
-            if ($check[0]["gen_res_own_id"]["own_phone_number"] != "" && $check[0]["gen_res_own_id"]["own_mobile_number"] != "")
+            if ($check["own_phone_number"] != "" && $check["own_mobile_number"] != "")
                 $data[4] .= " / ";
 
-            $data[4] .= $check[0]["gen_res_own_id"]["own_mobile_number"];
+            $data[4] .= $check["own_mobile_number"];
 
             //Total de habitaciones
-            $data[5] = $check[1];
+            $data[5] = $check["rooms"];
 
             //Total de huéspedes
-            $data[6] = $check[3] + $check[5];
+            $data[6] = $check["adults"] + $check["children"];
 
             //Noches
-            $data[7] = $total_nights[$check[0]["gen_res_id"]];
+            $data[7] = $check["nights"];
 
             //Fecha de Pago
-            $payDate = new \DateTime($check[7]);
+            $payDate = new \DateTime($check["payed"]);
             $data[8] = $payDate->format("d/m/Y");
 
             //Pago en casa
-            $data[9] = $check[0]["gen_res_total_in_site"] - $check[0]["gen_res_total_in_site"] * $check[0]["gen_res_own_id"]["own_commission_percent"] / 100;
+            $payAtService = $check["to_pay_at_service"] - $check["to_pay_at_service"] * $check["own_commission_percent"] / 100;
+            $data[9] = number_format((float)$payAtService, 2, '.', '');
             $data[9] .= " CUC";
             //Cliente
-            $data[10] = $check[0]["gen_res_user_id"]["user_user_name"] . " " . $check[0]["gen_res_user_id"]["user_last_name"];
-            $data[11] = $check[0]["gen_res_user_id"]["user_country"]["co_name"];
+            $data[10] = $check["user_user_name"] . " " . $check["user_last_name"];
+            $data[11] = $check["co_name"];
 
 
             array_push($results, $data);
