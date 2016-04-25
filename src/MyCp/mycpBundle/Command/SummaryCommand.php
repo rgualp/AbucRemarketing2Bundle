113b98019626bf79fb02c14bb4fc41dfdc4cd097
livan <livanvaldes084@gmail.com>
2016-04-25 17:02:11 -0400

    Summary dashboard

--- src/MyCp/mycpBundle/Command/SummaryCommand.php
+++ src/MyCp/mycpBundle/Command/SummaryCommand.php
@@ -99,7 +99,7 @@ class SummaryCommand extends ContainerAwareCommand
         try {
             $subject = "Sumario MyCasaParticular";
 
-            $emailService->sendEmail(array('damian.flores@mycasaparticular.com','ander@mycasaparticular.com'), $subject,  $body, 'no-responder@mycasaparticular.com');
+            $emailService->sendEmail(array('natalie@mycasaparticular.com','ptorres@mycasaparticular.com','anne.schweizer@abuc.ch','damian.flores@mycasaparticular.com','ander@mycasaparticular.com'), $subject,  $body, 'no-responder@mycasaparticular.com');
             $output->writeln('Successfully sent sales report email');
 
 
