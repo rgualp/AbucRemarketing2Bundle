887748a0b67f79b0db17f8dfd641ae7b9a756eb5
Arieskien Mendoza Guerra <arieskienmendoza@gmail.com>
2016-05-03 14:04:05 -0400

    Add i18n

--- src/hds/SeoBundle/Extension/SeoUtilsExtension.php
+++ src/hds/SeoBundle/Extension/SeoUtilsExtension.php
@@ -17,6 +17,11 @@ class SeoUtilsExtension extends \Twig_Extension
 	protected $block_repository;
 
 	/**
+	 * @var \hds\SeoBundle\Repository\BlockContent
+	 */
+	protected $blockcontent_repository;
+
+	/**
 	 * @var Request
 	 */
 	protected $request;
@@ -26,10 +31,11 @@ class SeoUtilsExtension extends \Twig_Extension
 	 *
 	 * @param ContainerInterface $container
 	 */
-	public function __construct($container, $block_repository)
+	public function __construct($container, $block_repository, $blockcontent_repository)
 	{
 		$this->container = $container;
 		$this->block_repository = $block_repository;
+		$this->blockcontent_repository= $blockcontent_repository;
 
 		if ($this->container->isScopeActive('request')) {
 			$this->request = $this->container->get('request');
@@ -53,7 +59,7 @@ class SeoUtilsExtension extends \Twig_Extension
 		);
 	}
 
-	public function getMetas($block_name, array $replacements=array()){
+	public function getMetas($block_name, $language_code, array $replacements=array()){
 		$block= $this->block_repository->findOneBy(array('name'=>$block_name));
 		if(!$block){
 			return '<!--- Seo: Bloque "'.$block_name.'" no existe!!! --->';
@@ -61,10 +67,14 @@ class SeoUtilsExtension extends \Twig_Extension
 		if(!$block->getIsActive()){
 			return '<!---Seo: Bloque "'.$block_name.'" esta desactivado!!! --->';
 		}
-		$block_contents= $block->getContents();
-		if(!$block_contents->count()){
-			return '<!---Seo: Bloque "'.$block_name.'" no tiene headers asociados!!! --->';
+		$block_contents= $this->blockcontent_repository->findBy(array(
+			'block'=>$block->getId(),
+			'language_code'=>$language_code
+		));
+		if(!count($block_contents)){
+			return '<!---Seo: Bloque "'.$block_name.'" con el idioma "'.$language_code.'" no tiene headers asociados!!! --->';
 		}
+
 
 		$headers= array();
 		$metas= '';
