07672151bc92a280f9fdd2ea4092542c847e0aba
Yanet Morales <yanet.moralesr@gmail.com>
2016-05-16 16:15:42 -0400

    Backend: Adding Destination column in client reservation's list details

--- src/MyCp/mycpBundle/Entity/generalReservationRepository.php
+++ src/MyCp/mycpBundle/Entity/generalReservationRepository.php
@@ -443,7 +443,8 @@ class generalReservationRepository extends EntityRepository {
             (SELECT SUM(owres3.own_res_count_childrens) FROM mycpBundle:ownershipReservation owres3 WHERE owres3.own_res_gen_res_id = gre.gen_res_id) AS childrens,
             (SELECT SUM(DATE_DIFF(owres5.own_res_reservation_to_date, owres5.own_res_reservation_from_date)) FROM mycpBundle:ownershipReservation owres5 WHERE owres5.own_res_gen_res_id = gre.gen_res_id) as totalNights,
             ow.own_mcp_code, ow.own_id, gre.gen_res_from_date,
-            (SELECT COUNT(ofl) from mycpBundle:offerLog ofl where ofl.log_offer_reservation = gre.gen_res_id) as isOffer, ow.own_inmediate_booking
+            (SELECT COUNT(ofl) from mycpBundle:offerLog ofl where ofl.log_offer_reservation = gre.gen_res_id) as isOffer, ow.own_inmediate_booking,
+            (SELECT MIN(des.des_name) FROM mycpBundle:destination des WHERE des.des_id = ow.own_destination) as destination
             FROM mycpBundle:generalReservation gre
             JOIN gre.gen_res_own_id ow
             JOIN gre.gen_res_user_id us
