376572806b6764db4487c192eaa431975c3b58a8
livan <livanvaldes084@gmail.com>
2016-05-19 17:09:55 -0400

    twig step

---
+++ src/MyCp/CasaModuleBundle/Resources/public/js/script/step4.js
@@ -0,0 +1,27 @@
+/*========================================================================
+ * App script to handle the entire CasaModule and base functions
+ * Copyright 2015.
+ *========================================================================*/
+var Step4 = function () {
+
+    /**
+     *
+     */
+    var addTab=function(){
+
+    }
+    return {
+        //main function to initiate template pages
+        init: function () {
+            //IMPORTANT!!!: Do not modify the call order.
+            addTab();
+        }
+    };
+}();
+//Start step1
+Step4.init();
+
+
+
+
+
