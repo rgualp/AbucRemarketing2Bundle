61287895e06d613d3e77aa0068afa7a4167c7d90
Yanet Morales <yanet.moralesr@gmail.com>
2016-05-23 18:04:35 -0400

    Step 5: Switch and change price jquery functionalities

--- src/MyCp/CasaModuleBundle/Resources/public/js/script/step5.js
+++ src/MyCp/CasaModuleBundle/Resources/public/js/script/step5.js
@@ -4,6 +4,106 @@
  *========================================================================*/
 var Step5 = function () {
 
+    var hasBreakfastVisibility = function(){
+        var hasBreakfast = $("#hasBreakfast").val();
+        if(hasBreakfast)
+        {
+            $(".facilitiesBreakfastWithPrice").css("display","block");
+            $(".facilitiesBreakfastPrice").css("display", "none");
+        }
+        else
+        {
+            $(".facilitiesBreakfastWithPrice").css("display","none");
+            $(".facilitiesBreakfastPrice").css("display", "block");
+        }
+    }
+
+    var hasDinnerVisibility = function(){
+        var hasDinner = $("#hasDinner").val();
+        if(hasDinner)
+        {
+            $(".facilitiesDinnerWithPrice").css("display","block");
+            $(".facilitiesDinnerPrice").css("display", "none");
+        }
+        else
+        {
+            $(".facilitiesDinnerWithPrice").css("display","none");
+            $(".facilitiesDinnerPrice").css("display", "block");
+        }
+    }
+
+    var hasParkingVisibility = function(){
+        var hasParking = $("#hasParking").val();
+        if(hasParking)
+        {
+            $(".facilitiesParkingWithPrice").css("display","block");
+            $(".facilitiesParkingPrice").css("display", "none");
+        }
+        else
+        {
+            $(".facilitiesParkingWithPrice").css("display","none");
+            $(".facilitiesParkingPrice").css("display", "block");
+        }
+    }
+
+    var showDivPrices=function(){
+        //Showing facilities prices divs
+        hasBreakfastVisibility();
+        hasDinnerVisibility();
+        hasParkingVisibility();
+    }
+
+    var changeBreakfastPrice = function(){
+        $(".facilitiesBreakfastWithPrice").css("display","none");
+        $(".facilitiesBreakfastPrice").css("display", "block");
+    }
+
+    var changeDinnerPrice = function(){
+        $(".facilitiesDinnerWithPrice").css("display","none");
+        $(".facilitiesDinnerPrice").css("display", "block");
+    }
+
+    var changeParkingPrice = function(){
+        $(".facilitiesParkingWithPrice").css("display","none");
+        $(".facilitiesParkingPrice").css("display", "block");
+    }
+
+    var switchBreakfast = function(){
+        var isChecked = $("#breakfast").is(':checked');
+
+        if(!isChecked){
+            $(".facilitiesBreakfastWithPrice").css("display","none");
+            $(".facilitiesBreakfastPrice").css("display", "none");
+        }
+        else{
+            hasBreakfastVisibility();
+        }
+    }
+
+    var switchDinner = function(){
+        var isChecked = $("#dinner").is(':checked');
+
+        if(!isChecked){
+            $(".facilitiesDinnerWithPrice").css("display","none");
+            $(".facilitiesDinnerPrice").css("display", "none");
+        }
+        else{
+            hasDinnerVisibility();
+        }
+    }
+
+    var switchParking = function(){
+        var isChecked = $("#parking").is(':checked');
+
+        if(!isChecked){
+            $(".facilitiesParkingWithPrice").css("display","none");
+            $(".facilitiesParkingPrice").css("display", "none");
+        }
+        else{
+            hasParkingVisibility();
+        }
+    }
+
     var saveStep5=function(){
         alert('Save form 5');
     }
@@ -14,7 +114,15 @@ var Step5 = function () {
             var event=App.getEvent();
             event.clickBtnContinueAfter.add(saveStep5,this);
 
+            showDivPrices();
 
+            $("#btn-change-breakfast").click(changeBreakfastPrice);
+            $("#btn-change-dinner").click(changeDinnerPrice);
+            $("#btn-change-parking").click(changeParkingPrice);
+
+            $("#breakfast").change(switchBreakfast);
+            $("#dinner").change(switchDinner);
+            $("#parking").change(switchParking);
 
         }
 
