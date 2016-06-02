a6fd0ea36843926633e819bd7f5bd811c833c710
livan <livanvaldes084@gmail.com>
2016-06-02 16:17:48 -0400

    Nuevo dashboard

--- src/MyCp/CasaModuleBundle/Controller/DefaultController.php
+++ src/MyCp/CasaModuleBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use MyCp\CasaModuleBundle\Form\ownershipStepPhotosType;
 use MyCp\mycpBundle\Entity\ownershipPhoto;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use MyCp\CasaModuleBundle\Form\ownershipStep1Type;
+use MyCp\mycpBundle\Entity\ownershipStatus;
 
 class DefaultController extends Controller
 {
@@ -15,15 +16,18 @@ class DefaultController extends Controller
         if(empty($user->getUserUserCasa()))
             return new NotFoundHttpException('El usuario no es usuario casa');
         $ownership=  $user->getUserUserCasa()[0]->getUserCasaOwnership();
-        $form=$this->createForm(new ownershipStep1Type(),$ownership);
-//        $owp= new ownershipPhoto();
-//        $owp->setOwnPhoOwn($ownership);
-//        $ownership->addPhoto($owp);
-        $photosForm=$this->createForm(new ownershipStepPhotosType(),$ownership,array( 'action' => $this->generateUrl('save_step6'), 'attr' =>['id'=>'mycp_mycpbundle_ownership_step_photos']));
-        return $this->render('MyCpCasaModuleBundle:Default:index.html.twig', array(
-            'ownership'=>$ownership,
-            'form'=>$form->createView(),
-            'photoForm'=>$photosForm->createView()
-        ));
+        if($ownership->getOwnStatus()->getStatusId()==ownershipStatus::STATUS_ACTIVE){
+            return $this->render('MyCpCasaModuleBundle:Default:dashboard.html.twig', array());
+        }
+        else{
+            $form=$this->createForm(new ownershipStep1Type(),$ownership);
+            $photosForm=$this->createForm(new ownershipStepPhotosType(),$ownership,array( 'action' => $this->generateUrl('save_step6'), 'attr' =>['id'=>'mycp_mycpbundle_ownership_step_photos']));
+            return $this->render('MyCpCasaModuleBundle:Default:register.html.twig', array(
+                'ownership'=>$ownership,
+                'form'=>$form->createView(),
+                'photoForm'=>$photosForm->createView()
+            ));
+        }
+
     }
 }
