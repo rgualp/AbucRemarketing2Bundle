33559989b1aae1bd2a4ab46edd7dbf9010e10a6f
Yanet Morales <yanet.moralesr@gmail.com>
2016-06-22 14:19:04 -0400

    Task 553: Asociar reservas y carritos actuales a la tarifa de precios inicial.

---
+++ app/migrations/Version20160622200906.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160622200906 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("set @serviceFee = (SELECT MAX(id) from servicefee where fixedFee = 10 order by date ASC)");
+
+        $this->addSql("update cart set service_fee = @serviceFee");
+        $this->addSql("update generalreservation set service_fee = @serviceFee");
+
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("update cart set service_fee is null");
+        $this->addSql("update generalreservation set service_fee  is null");
+    }
+}
