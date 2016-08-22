e71c8241640c0670adde393623a24970f15677cc
Alejandro Capote <livanvaldes084@gmail.com>
2016-08-22 09:53:06 -0400

    index

---
+++ src/MyCp/PartnerBundle/Controller/FrontendController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyCp\PartnerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("partner")
+ */
+class FrontendController extends Controller
+{
+    /**
+     * @Route("/", name="partner_home")
+     */
+    public function indexAction()
+    {
+        return $this->render('PartnerBundle:Frontend:index.html.twig');
+    }
+}
