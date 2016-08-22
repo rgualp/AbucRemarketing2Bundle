f325dcd07efcee183e2ace10bfce0ab6dee188c7
Alejandro Capote <livanvaldes084@gmail.com>
2016-08-22 10:01:42 -0400

    index

--- src/MyCp/PartnerBundle/Controller/FrontendController.php
+++ src/MyCp/PartnerBundle/Controller/FrontendController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * @Route("partner")
+ * @Route("/{_locale}/partner")
  */
 class FrontendController extends Controller
 {
