7de310fc83facd01c5f829ed9637456c7b665916
Alejandro Capote <livanvaldes084@gmail.com>
2016-08-25 08:51:46 -0400

    Firewall an Backend

--- src/MyCp/PartnerBundle/Controller/SecurityController.php
+++ src/MyCp/PartnerBundle/Controller/SecurityController.php
@@ -10,21 +10,29 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends Controller
 {
-    public function loginAction($urlLogin, Request $request)
+    public function loginAction(Request $request)
     {
         $session = $request->getSession();
-        $request->setLocale('es');
+
+        // get the login error if there is one
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
         } else {
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
             $session->remove(SecurityContext::AUTHENTICATION_ERROR);
         }
-        return $this->render('LayoutBundle:Security:login.html.twig', array(
-            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-            'error' => $error,
-            'urlLogin' => $urlLogin,
-        ));
+
+        return $this->render(
+            'LayoutBundle:Security:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+                'btn_close'=>false
+            )
+        );
     }
 
    /* public function forgotPasswordAction(Request $request)
