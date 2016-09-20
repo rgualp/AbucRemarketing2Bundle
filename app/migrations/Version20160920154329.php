2cdd2e71c0200bdeeeadbb2c0e6a7645b1b6ccc8
Miguel Gómez León <mgleonsc@gmail.com>
2016-09-20 17:32:40 -0400

    reserved

--- app/migrations/Version20160920154329.php
+++ app/migrations/Version20160920154329.php
@@ -17,20 +17,20 @@ class Version20160920154329 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE `mycasapa_prod`.`user`
+        $this->addSql('ALTER TABLE `user`
                         ADD COLUMN `user_currency` INT(45) NULL AFTER `locked`,
                         ADD COLUMN `user_language` INT(45) NULL AFTER `user_currency`,
                         ADD INDEX `fk_user_currency_idx` (`user_currency` ASC),
                         ADD INDEX `fk_user_language_idx` (`user_language` ASC);
-                        ALTER TABLE `mycasapa_prod`.`user`
+                        ALTER TABLE `user`
                         ADD CONSTRAINT `fk_user_currency`
                           FOREIGN KEY (`user_currency`)
-                          REFERENCES `mycasapa_prod`.`currency` (`curr_id`)
+                          REFERENCES `currency` (`curr_id`)
                           ON DELETE NO ACTION
                           ON UPDATE NO ACTION,
                         ADD CONSTRAINT `fk_user_language`
                           FOREIGN KEY (`user_language`)
-                          REFERENCES `mycasapa_prod`.`lang` (`lang_id`)
+                          REFERENCES `lang` (`lang_id`)
                           ON DELETE NO ACTION
                           ON UPDATE NO ACTION');
     }
