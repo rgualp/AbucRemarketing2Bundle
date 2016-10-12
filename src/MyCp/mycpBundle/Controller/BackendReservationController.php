86925b09538f77fb7b6ce8aced171e14cbb15677
Yanet Morales <yanet.moralesr@gmail.com>
2016-10-12 17:44:17 -0400

    Resolving issue with tourist tax in voucher

--- src/MyCp/mycpBundle/Controller/BackendReservationController.php
+++ src/MyCp/mycpBundle/Controller/BackendReservationController.php
@@ -66,7 +66,8 @@ class BackendReservationController extends Controller {
                 $arrayNightsByOwnershipReservation = $resultReservations['nights'];
                 $general_reservation = $resultReservations['generalReservation'];
 
-                $general_reservation->setServiceFee($gen_res->getServiceFee());
+                $serviceFee = $gen_res->getServiceFee();
+                $general_reservation->setServiceFee($serviceFee);
 
                 //Deleting booking association from cancelled and payed reservation
                 foreach ($reservations as $res) {
--- src/MyCp/mycpBundle/Helpers/VoucherHelper.php
+++ src/MyCp/mycpBundle/Helpers/VoucherHelper.php
@@ -56,6 +56,9 @@ class VoucherHelper {
             foreach ($bookings_ids as $bookId) {
                 $bookId = $bookId['booking_id'];
 
+                /*var_dump($createVoucher);
+                die;*/
+
                 $pdfFilePath = "";
                 if($createVoucher)
                     $pdfFilePath = $bookingService->createBookingVoucher($bookId);
