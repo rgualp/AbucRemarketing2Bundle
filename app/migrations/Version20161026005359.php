8fecf9a40e2642dec17bc466dcad1a4236b21d89
Yanet Morales <yanet.moralesr@gmail.com>
2016-10-31 11:39:23 -0400

    Updating inmediate booking data

--- app/migrations/Version20161026005359.php
+++ app/migrations/Version20161026005359.php
@@ -21,9 +21,7 @@ class Version20161026005359 extends AbstractMigration
 
         $this->addSql('UPDATE ownership
                     SET own_inmediate_booking_2 = 1
-                    WHERE own_mcp_code IN ("CH1486", "CH005", "CH1388", "CH1368", "CH1366", "CH1277", "CH1048",
-                    "CH1139", "CH087", "CH1416", "CH671", "CH1327", "CH1343", "CH1502", "CH1321", "CH1341", "CH1347",
-                    "CH1495", "CH1418", "CH1420");');
+                    WHERE own_mcp_code IN ("CH1486", "CH005", "CH1388", "CH1368", "CH1366", "CH1277", "CH1048", "CH087", "CH1327", "CH1343", "CH1418", "CH1420");');
 
     }
 
@@ -36,9 +34,7 @@ class Version20161026005359 extends AbstractMigration
 
         $this->addSql('UPDATE ownership
                     SET own_inmediate_booking_2 = 0
-                    WHERE own_mcp_code IN ("CH1486", "CH005", "CH1388", "CH1368", "CH1366", "CH1277", "CH1048",
-                    "CH1139", "CH087", "CH1416", "CH671", "CH1327", "CH1343", "CH1502", "CH1321", "CH1341", "CH1347",
-                    "CH1495", "CH1418", "CH1420");');
+                    WHERE own_mcp_code IN ("CH1486", "CH005", "CH1388", "CH1368", "CH1366", "CH1277", "CH1048", "CH087", "CH1327", "CH1343", "CH1418", "CH1420");');
 
     }
 }
