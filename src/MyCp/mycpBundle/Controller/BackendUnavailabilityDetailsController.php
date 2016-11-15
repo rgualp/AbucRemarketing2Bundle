e067530463022c070d8c8e6a6bae6e388ece46ec
Yanet Morales <yanet.moralesr@gmail.com>
2016-11-15 16:02:29 -0500

    Disponibilidad: Creando servicio para agregar no disponibilidades

--- src/MyCp/mycpBundle/Controller/BackendUnavailabilityDetailsController.php
+++ src/MyCp/mycpBundle/Controller/BackendUnavailabilityDetailsController.php
@@ -155,67 +155,9 @@ class BackendUnavailabilityDetailsController extends Controller {
                 if ($date_from > $date_to) {
                     $this->get('session')->getFlashBag()->add('message_error_main', "La fecha Desde tiene que ser menor o igual que la fecha Hasta");
                 } else {
-                    $unavailabilityDetail = array();
-                    $unavailabilityDetail["start"] = date('Y-m-d',$date_from->getTimestamp());
-                    $unavailabilityDetail["end"] = date('Y-m-d', $date_to->getTimestamp());
-                    $unavailabilityDetail["reason"] = $post_form['ud_reason'];
-
-                    $intervals = array();
-
-                    $reservations = $em->getRepository('mycpBundle:ownershipReservation')->getReservationReservedByRoomAndDate($id_room, $date_from->format('Y-m-d'), $date_to->format('Y-m-d'), true);
-
-                    $index = 0;
-                    $startDate = $date_from;
-                    //$endDate = $date_to;
-                    foreach($reservations as $reservation)
-                    {
-                        $index++;
-                        //La fecha fin del intervalo sera la inicial de la proxima reserva o
-                        $endDate = (count($reservations) > $index) ? $reservations[$index]["own_res_reservation_from_date"] : $date_to;
-
-                        //La fecha de inicio de la reserva es menor que la de inicio del rango y la fecha fin de la reserva esta en el rango
-                        if($reservation["own_res_reservation_from_date"] < $startDate && $reservation["own_res_reservation_to_date"] >= $startDate && $reservation["own_res_reservation_to_date"] < $endDate)
-                        {
-                            if($reservation["own_res_reservation_to_date"] != $endDate)
-                                $intervals[] = array("start" => $reservation["own_res_reservation_to_date"] , "end" => $endDate);
-                            $startDate = $endDate;
-                        }
-                        //si la reserva esta dentro del rango
-                        elseif($reservation["own_res_reservation_from_date"] >= $startDate && $reservation["own_res_reservation_to_date"] <= $endDate)
-                        {
-                            $dateBefore = date_modify($reservation["own_res_reservation_from_date"], "-1 day");
-
-                            if($startDate != $dateBefore)
-                                $intervals[] = array("start" => $startDate, "end" => $dateBefore);
-
-                            if($reservation["own_res_reservation_to_date"] != $endDate)
-                                $intervals[] = array("start" => $reservation["own_res_reservation_to_date"], "end" => $endDate);
-                            $startDate = $endDate;
-                        }
-                        //La fecha de inicio de la reserva esta en el rango pero la fecha fin de la reserva es mayor que la fecha fin del rango
-                        elseif($reservation["own_res_reservation_to_date"] > $endDate && $reservation["own_res_reservation_from_date"] >= $startDate && $reservation["own_res_reservation_from_date"] <= $endDate)
-                        {
-                            $dateBefore = date_modify($reservation["own_res_reservation_from_date"], "-1 day");
-
-                            if($startDate != $dateBefore)
-                                $intervals[] = array("start" => $startDate, "end" => $dateBefore);
-                            $startDate = $reservation["own_res_reservation_from_date"];
-                        }
-                        else
-                            $startDate = $reservation["own_res_reservation_to_date"];
-
-                    }
-
-                    if(count($reservations) == 0)
-                        $intervals[] = array("start" => $date_from, "end" => $date_to);
-
-                    //var_dump($intervals); die;
-
-                    $unavailabilityDetail["date_range"] = $intervals;
-                    //Utils::debug($unavailabilityDetail); die;
-
-                    $em->getRepository('mycpBundle:unavailabilityDetails')->addAvailableRoomByRange($unavailabilityDetail, $id_room);
-
+                    //$post_form['ud_reason']
+                    $udetailsService = $this->get('mycp.udetails.service');
+                    $udetailsService->addUDetail($id_room, $date_from, $date_to, $post_form['ud_reason']);
 
                     /*$unavailabilities = $em->getRepository('mycpBundle:unavailabilityDetails')->getAllNotDeletedByDateAndRoom($id_room, $date_from->format('Y-m-d'), $date_to->format('Y-m-d'));
                     $reservations = $em->getRepository('mycpBundle:ownershipReservation')->getReservationReservedByRoomAndDate($id_room, $date_from->format('Y-m-d'), $date_to->format('Y-m-d'));
