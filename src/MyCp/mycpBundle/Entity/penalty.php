a143548f865b0ab676e7eecd03644a06f694f04b
Yanet Morales <yanet.moralesr@gmail.com>
2016-11-25 15:21:38 -0500

    Ranking: Adding penalty entity, migration and relations with entities ownership and user

---
+++ src/MyCp/mycpBundle/Entity/penalty.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace MyCp\mycpBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * log
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class penalty
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="user",inversedBy="createdPenalties")
+     * @ORM\JoinColumn(name="user",referencedColumnName="user_id")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ownership",inversedBy="penalties")
+     * @ORM\JoinColumn(name="accommodation",referencedColumnName="own_id")
+     */
+    private $accommodation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creationDate", type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="finalizationDate", type="datetime")
+     */
+    private $finalizationDate;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccommodation()
+    {
+        return $this->accommodation;
+    }
+
+    /**
+     * @param mixed $accommodation
+     * @return mixed
+     */
+    public function setAccommodation($accommodation)
+    {
+        $this->accommodation = $accommodation;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @param \DateTime $creationDate
+     * @return mixed
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return mixed
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getFinalizationDate()
+    {
+        return $this->finalizationDate;
+    }
+
+    /**
+     * @param \DateTime $finalizationDate
+     * @return mixed
+     */
+    public function setFinalizationDate($finalizationDate)
+    {
+        $this->finalizationDate = $finalizationDate;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     * @return mixed
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+
+
+}
