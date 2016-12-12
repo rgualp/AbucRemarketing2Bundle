9002fb809d1a52a06cf208d4064f7d24047cd0ca
Ernesto Gonzalez <ernesto@hds.li>
2016-12-12 15:48:29 -0500

    Fixed Error Translation Redirecting

--- src/MyCp/FrontEndBundle/Form/changePasswordUserType.php
+++ src/MyCp/FrontEndBundle/Form/changePasswordUserType.php
@@ -22,12 +22,11 @@ class changePasswordUserType extends AbstractType
     {
         $builder
             ->add('user_password','repeated',array(
-            'first_name' => $this->translate->trans('FORMS_PASSWORD'),
-            'second_name' => $this->translate->trans('FORMS_REPEAT'),
-            'type' => 'password',
-            'constraints'=>array(new NotBlank(), new Length(array('min'=>6))),
-        ));
-        ;
+                'first_options' => array('label' => $this->translate->trans('FORMS_PASSWORD')),
+                'second_options' => array('label' =>$this->translate->trans('FORMS_REPEAT')),
+                'type' => 'password',
+                'constraints'=>array(new NotBlank(), new Length(array('min'=>6))),
+            ));
     }
 
     public function getName()
