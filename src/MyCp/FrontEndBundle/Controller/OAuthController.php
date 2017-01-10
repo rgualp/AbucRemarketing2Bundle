6a26886d6c2a4ddc61c3a1e2ff228341931b929c
Yanet Morales <yanet.moralesr@gmail.com>
2017-01-10 11:49:37 -0500

    Notifications config

--- src/MyCp/FrontEndBundle/Controller/OAuthController.php
+++ src/MyCp/FrontEndBundle/Controller/OAuthController.php
@@ -105,7 +105,7 @@ class OAuthController extends Controller
                             $em->persist($userTourist);
 
 
-                           /* $hash_user = hash('sha256', $fbLoginData->getName());
+                            $hash_user = hash('sha256', $fbLoginData->getName());
                             $hash_email = hash('sha256', strtolower($fbLoginData->getEmail()));
                             $password="";
                             //Registrando al user en HDS-MEAN
@@ -123,7 +123,7 @@ class OAuthController extends Controller
                             // cerramos la sesión cURL
                             curl_close ($ch);
                             $user->setRegisterNotification(true);
-                            $em->persist($user);*/
+                            $em->persist($user);
                             $em->flush();
                         }
 
@@ -208,7 +208,7 @@ class OAuthController extends Controller
                             }
                         }
 
-                        /*$hash_user = hash('sha256', $user->getUserUserName());
+                        $hash_user = hash('sha256', $user->getUserUserName());
                         $hash_email = hash('sha256', $user->getUserEmail());
                         //-----------------Autenticando al usuario en HDS-MEN
                         $session = $this->container->get('session');
@@ -229,7 +229,7 @@ class OAuthController extends Controller
                             $user->setOnline(true);
                             $em->persist($user);
                             $em->flush();
-                        }*/
+                        }
 
                     }
                     else{
