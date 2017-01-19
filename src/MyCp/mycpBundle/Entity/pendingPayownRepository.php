52ed057eea85c3bc6cdbbf0fa3b6916eb47e2b51
Yanet Morales <yanet.moralesr@gmail.com>
2017-01-19 17:19:18 -0500

    Cancel payment

--- src/MyCp/mycpBundle/Entity/pendingPayownRepository.php
+++ src/MyCp/mycpBundle/Entity/pendingPayownRepository.php
@@ -18,9 +18,37 @@ class pendingPayownRepository extends EntityRepository {
         $qb = $em->createQueryBuilder()
             ->select("op")
             ->from("mycpBundle:pendingPayown", "op")
+            ->join("op.user_casa", "acc")
+            ->join("op.type", "type")
             ->orderBy("op.pending_id", "DESC")
             ->orderBy("op.payment_date", "DESC");
 
+        if($filter_number != null && $filter_number != "" && $filter_number != "null")
+        {
+            $qb->andWhere("op.pending_id LIKE :pending_id")
+                ->setParameter("pending_id", '%'.$filter_number.'%');
+        }
+        if($filter_code != null && $filter_code != "" && $filter_code != "null")
+        {
+            $qb->andWhere("acc.own_mcp_code LIKE :code")
+                ->setParameter("code", '%'.$filter_code.'%');
+        }
+        if($filter_method != null && $filter_method != "" && $filter_method != "null")
+        {
+            $qb->andWhere("type.nom_id = :type")
+                ->setParameter("type", $filter_method);
+        }
+        if($filter_payment_date_from != null && $filter_payment_date_from != "" && $filter_payment_date_from != "null")
+        {
+            $qb->andWhere("op.payment_date >= :dateFrom")
+                ->setParameter("dateFrom", $filter_payment_date_from);
+        }
+
+        if($filter_payment_date_to != null && $filter_payment_date_to != "" && $filter_payment_date_to != "null")
+        {
+            $qb->andWhere("op.payment_date <= :dateTo")
+                ->setParameter("dateTo", $filter_payment_date_to);
+        }
         return $qb->getQuery();
 
     }
