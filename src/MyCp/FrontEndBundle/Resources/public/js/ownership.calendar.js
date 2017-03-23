985fd2876524059e0b5af1f95605ee7a2fe3d13c
Yanet Morales <yanet.moralesr@gmail.com>
2017-03-23 18:27:56 -0400

    Soporte #289: Recalcular precios si la cantidad de personas es mayor o igual a tres y mostrar cambio de precios en la fila correspondiente a la habitacion triple

--- src/MyCp/FrontEndBundle/Resources/public/js/ownership.calendar.js
+++ src/MyCp/FrontEndBundle/Resources/public/js/ownership.calendar.js
@@ -226,13 +226,17 @@ function reservations_in_details()
                 persons=parseInt($('#combo_kids_'+$(this).attr('data')).val()) + parseInt($('#combo_guest_'+$(this).attr('data')).val());
                 console.log("Antes de ver el up 2");
                 isUp2($(this).attr("name"), $(this).attr('data'));
-                if($(this).attr('data_is_triple')==='true' && persons>=3)
+                if(($(this).attr('data_is_triple')==='1' || $(this).attr('data_is_triple')==='true') && persons>=3)
                 {
                     value=$(this).attr('data_total')*$(this).attr('data_curr') + (($(this).attr('data_curr')*$(this).attr('data_triple_recharge')) * (cont_array_dates -1));
+                    $('.normalPrice_' + $(this).attr('data')).css({display: 'none'});
+                    $('.triplePrice_' + $(this).attr('data')).css({display: 'block'});
                 }
                 else
                 {
                     value=$(this).attr('data_total')*$(this).attr('data_curr');
+                    $('.normalPrice_' + $(this).attr('data')).css({display: 'block'});
+                    $('.triplePrice_' + $(this).attr('data')).css({display: 'none'});
                 }
                 value= normalize_prices(value);
                 $('#guest_'+$(this).attr('data')).html($('#combo_guest_'+$(this).attr('data')).val());
@@ -248,13 +252,17 @@ function reservations_in_details()
             value=0;
             real_value=0;
             persons=parseInt($('#combo_kids_'+$(this).attr('data')).val()) + parseInt($('#combo_guest_'+$(this).attr('data')).val());
-            if($(this).attr('data_is_triple')==='true' && persons>=3)
+            if(($(this).attr('data_is_triple')==='1' || $(this).attr('data_is_triple')==='true') && persons>=3)
             {
                 value=$(this).attr('data_total')*$(this).attr('data_curr') +(($(this).attr('data_curr')*$(this).attr('data_triple_recharge')) * (cont_array_dates -1)) ;
+                $('.normalPrice_' + $(this).attr('data')).css({display: 'none'});
+                $('.triplePrice_' + $(this).attr('data')).css({display: 'block'});
             }
             else
             {
                 value=$(this).attr('data_total')*$(this).attr('data_curr');
+                $('.normalPrice_' + $(this).attr('data')).css({display: 'block'});
+                $('.triplePrice_' + $(this).attr('data')).css({display: 'none'});
             }
 
             value= normalize_prices(value);
