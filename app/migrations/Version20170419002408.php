a6b510489416bc5953dd3e723bfc71f20489d713
Yanet Morales <yanet.moralesr@gmail.com>
2017-04-18 18:26:59 -0400

    Soporte #328

---
+++ app/migrations/Version20170419002408.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170419002408 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("update ownership o
+                       set o.own_minimum_price = (SELECT MIN(r.room_price_down_to) from room r
+                                                  where r.room_ownership = o.own_id);");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
