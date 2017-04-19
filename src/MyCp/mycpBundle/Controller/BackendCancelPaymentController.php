b364a3177300fc5f92a276eb14104bcd69ccf53a
Livan Capote <livanvaldes084@gmail.com>
2017-04-19 14:41:59 -0400

    Add email yander

--- src/MyCp/mycpBundle/Controller/BackendCancelPaymentController.php
+++ src/MyCp/mycpBundle/Controller/BackendCancelPaymentController.php
@@ -275,7 +275,7 @@ class BackendCancelPaymentController extends Controller {
                         'reason'=>$cancelPayment->getReason(),
                         'payment_date'=>$pay_own->getPaymentDate()
                     ));
-                 $emailService->sendEmail(array("reservation@mycasaparticular.com","sarahi_amor@yahoo.com","andy.cabrera08@gmail.com"),"Pago Pendiente a Propietario:",$body,"no-reply@mycasaparticular.com");
+                 $emailService->sendEmail(array("reservation@mycasaparticular.com","yander@hds.li","andy.cabrera08@gmail.com"),"Pago Pendiente a Propietario:",$body,"no-reply@mycasaparticular.com");
                 //$emailService->sendEmail(array("damian.flores@mycasaparticular.com","andy.cabrera08@gmail.com"),"Pago Pendiente a Propietario:",$body,"no-reply@mycasaparticular.com");
             }
 
