133775506f97562c63f0181f7bc9e012f8fbc7f0
Livan Capote <livanvaldes084@gmail.com>
2017-05-16 13:58:11 -0400

    Fixed date

--- src/MyCp/FrontEndBundle/Controller/OwnershipController.php
+++ src/MyCp/FrontEndBundle/Controller/OwnershipController.php
@@ -40,9 +40,9 @@ class OwnershipController extends Controller
 
         $nights = $timer->nights($dateFrom->getTimestamp(), $dateTo->getTimestamp());
         $dateTo->setTimestamp(strtotime("-1 day", $end_timestamp));
-        if($dateFrom==$dateTo){
+       /* if($dateFrom==$dateTo){
             $dateTo->setTimestamp(strtotime("+1 day", $end_timestamp));
-        }
+        }*/
         if (!$owner_id) {
             throw $this->createNotFoundException();
         }
