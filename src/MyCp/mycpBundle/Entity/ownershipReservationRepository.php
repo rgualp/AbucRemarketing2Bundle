30bf0373a2660ffecef9dd49d8f984c68a0c4963
Livan Capote <livanvaldes084@gmail.com>
2017-05-23 12:38:46 -0400

    Fixed date

--- src/MyCp/mycpBundle/Entity/ownershipReservationRepository.php
+++ src/MyCp/mycpBundle/Entity/ownershipReservationRepository.php
@@ -513,7 +513,8 @@ class ownershipReservationRepository extends EntityRepository {
         WHERE (ore.own_res_status = $reservedCode)
         AND ((ore.own_res_reservation_from_date >= :start AND ore.own_res_reservation_to_date < :end) OR
              (ore.own_res_reservation_to_date > :start AND ore.own_res_reservation_to_date < :end) OR
-             (ore.own_res_reservation_from_date <= :end AND ore.own_res_reservation_from_date >= :start))
+             (ore.own_res_reservation_from_date <= :end AND ore.own_res_reservation_from_date >= :start) OR
+             (ore.own_res_reservation_from_date <= :start AND ore.own_res_reservation_to_date > :start))
         AND ore.own_res_selected_room_id = :room_id
         ORDER BY ore.own_res_reservation_from_date ASC");
         return $query->setParameter('start', $startParam)->setParameter('end', $endParam)->setParameter('room_id', $roomId)->getResult();
