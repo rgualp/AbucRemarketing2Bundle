ce571cd514b5e606c6d3ad654ab4ef222b326041
Yanet Morales <yanet.moralesr@gmail.com>
2017-11-12 17:35:50 -0500

    Habilitar el Plan: “Especial” en Partner

---
+++ app/migrations/Version20171112233336.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171112233336 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("insert into pa_package(name, completePayment) values ('Especial', 1)");
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
