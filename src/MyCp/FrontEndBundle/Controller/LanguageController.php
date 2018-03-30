c2fbc92a6dcf1701d1a2679ef1d05edcf8b823cf
Rolando Gual <rolandogual@gmail.com>
2018-03-30 10:32:52 -0400

    UPDATED: Actualizando la gestion de idiomas.

--- src/MyCp/FrontEndBundle/Controller/LanguageController.php
+++ src/MyCp/FrontEndBundle/Controller/LanguageController.php
@@ -3,43 +3,12 @@
 namespace MyCp\FrontEndBundle\Controller;
 
 
-use BeSimple\I18nRoutingBundle\Tests\Routing\RouterTest;
-use MyCp\FrontEndBundle\Helpers\Utils;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class LanguageController extends Controller
 {
 
-    public function setLocale($newLocale ) {
-        $request = $this->getRequest();
-
-        // get last requested path
-        $referer = $request->headers->get('referer');
-        $logger = $this->container->get('logger');
-        $logger->warning($this->getRequest()->getHost());
-        //get Host
-        $lastPath = substr($referer, strpos($referer, $request->getHost()));
-        $lastPath = str_replace($request->getHost(),'', $lastPath);
-
-        // get last route
-        $matcher = $this->get('router');
-        $parameters = $matcher->match($lastPath);
-
-        // set new locale (to session and to the route parameters)
-        $parameters['_locale'] = $newLocale;
-        $parameters['locale']=$newLocale;
-
-        // default parameters has to be unsetted!
-        $route = $parameters['_route'];
-        unset($parameters['_route']);
-        unset($parameters['_controller']);
-
-
-        return $this->generateUrl($route, $parameters);
-    }
     public function getLanguagesAction($route, $routeParams = null)
     {
         $routeParams = empty($routeParams) ? array() : $routeParams;
@@ -49,9 +18,6 @@ class LanguageController extends Controller
         $languages = $em
             ->getRepository('mycpBundle:lang')
             ->findBy(array('lang_active' => 1), array('lang_name' => 'ASC'));
-
-
-
 
 
 //        if ($route == 'frontend_details_destination'){
@@ -97,10 +63,10 @@ class LanguageController extends Controller
             ->findBy(array('lang_active' => 1), array('lang_name' => 'ASC'));
 
         $response = $this->render('FrontEndBundle:language:languages.html.twig', array(
-                'languages' => $languages,
-                'route' => $route,
-                'routeParams' => $routeParams
-            ));
+            'languages' => $languages,
+            'route' => $route,
+            'routeParams' => $routeParams
+        ));
 
         // cache controol -> languages rarely change
         $response->setSharedMaxAge(3600);
@@ -111,15 +77,10 @@ class LanguageController extends Controller
     public function changeAction($lang, $route, $routeParams = null)
     {
         $em = $this->getDoctrine()->getManager();
-
-
-
-        $newlang=$lang;
-
+        $newlang = $lang;
         //Guardar en userTourist el lenguaje q cambio
         $lang = $em->getRepository('mycpBundle:lang')->findOneBy(array('lang_code' => $lang));
         $user = $this->getUser();
-
         if (!empty($user) && $user != 'anon.') {
             $userTourist = $em
                 ->getRepository('mycpBundle:userTourist')
@@ -127,14 +88,41 @@ class LanguageController extends Controller
 
             // if the user is not a tourist (e.g. a staff member), the
             // userTourist does not exist
-            if(isset($userTourist)) {
+            if (isset($userTourist)) {
                 $userTourist->setUserTouristLanguage($lang);
                 $em->persist($userTourist);
                 $em->flush();
             }
         }
 
-
         return $this->redirect($this->setLocale($newlang));
+    }
+
+    public function setLocale($newLocale)
+    {
+        $request = $this->get('request');
+
+        // get last requested path
+        $referer = $request->headers->get('referer');
+        $logger = $this->container->get('logger');
+        $logger->warning($this->get('request')->getHost());
+        $parameters = array();
+        if (!is_null($referer) && $referer != "") {
+            //get Host
+            $lastPath = substr($referer, strpos($referer, $request->getHost()));
+            $lastPath = str_replace($request->getHost(), '', $lastPath);
+            $lastPath = str_replace('/app_dev.php/', '/', $lastPath);
+            $matcher = $this->get('router');
+            $parameters = $matcher->match($lastPath);
+        }
+
+        // set new locale (to session and to the route parameters)
+        $parameters['_locale'] = $newLocale;
+        $parameters['locale'] = $newLocale;
+        $route = $parameters['_route'];
+
+        unset($parameters['_route']);
+        unset($parameters['_controller']);
+        return $this->generateUrl($route, $parameters);
     }
 }
