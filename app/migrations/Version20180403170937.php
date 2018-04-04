9424049a8112ef99cc4f17eaeb33e11971cc2c79
Rolando Gual <rolandogual@gmail.com>
2018-04-04 12:02:52 -0400

    UPDATE: Gestionando los las provincias [Accomodations]

---
+++ app/migrations/Version20180403170937.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180403170937 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema)
+    {
+
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("ALTER TABLE province ADD enabled TINYINT(1) DEFAULT NULL;");
+        $this->addSql("UPDATE province SET  enabled = '0';");
+        $this->addSql("ALTER TABLE province CHANGE enabled enabled TINYINT(1) NOT NULL;");
+
+
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    }
+}
