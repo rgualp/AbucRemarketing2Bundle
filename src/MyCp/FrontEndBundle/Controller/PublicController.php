4ee32244d9773aecde296b5408c2570b5e5aa172
Orlando <oviera@uci.cu>
2018-07-01 00:55:43 -0400

    changes

--- src/MyCp/FrontEndBundle/Controller/PublicController.php
+++ src/MyCp/FrontEndBundle/Controller/PublicController.php
@@ -94,7 +94,7 @@ class PublicController extends Controller {
                 'locale' => $glogal_locale,
                 'provinces' => $provinces,
                 'slide_folder' => $slide_folder,
-
+                'own_top20_list' => $own_top20_list,
                 'premium_total' => $statistics['premium_total'],
                 'midrange_total' => $statistics['midrange_total'],
                 'economic_total' => $statistics['economic_total'],
