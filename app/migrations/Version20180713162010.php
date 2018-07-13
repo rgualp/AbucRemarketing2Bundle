b2cb5fd45c27ca0f88eb32bc87b12fe05f4af200
Rolando Gual <rolandogual@gmail.com>
2018-07-13 16:26:36 -0400

    ADD: Mostrando icono de desayuno incluido.

---
+++ app/migrations/Version20180713162010.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyCp\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180713162010 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE ownership ADD own_facilities_breakfast_include TINYINT(1) NOT NULL');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
