48555f7a380919c1eb3eded631165ca264473f8c
Orlando <vhagar91@gmail.com>
2018-09-17 15:34:42 -0400

    changes

--- src/MyCp/mycpBundle/Entity/transferRepository.php
+++ src/MyCp/mycpBundle/Entity/transferRepository.php
@@ -9,13 +9,7 @@
 namespace MyCp\mycpBundle\Entity;
 use Doctrine\ORM\EntityRepository;
 
-use Doctrine\ORM\Mapping as ORM;
-/**
- * transfer
- *
- * @ORM\Table(name="transfers")
- * @ORM\Entity(repositoryClass="MyCp\mycpBundle\Entity\transfer")
- */
+
 class transferRepository extends EntityRepository
 {
 
