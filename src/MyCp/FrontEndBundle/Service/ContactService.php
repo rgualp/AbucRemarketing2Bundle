e5021327abd02bd0614f3728e54e23ae31b51152
Orlando <vhagar91@gmail.com>
2018-09-20 15:35:54 -0400

    contact

--- src/MyCp/FrontEndBundle/Service/ContactService.php
+++ src/MyCp/FrontEndBundle/Service/ContactService.php
@@ -118,8 +118,8 @@ class ContactService extends Controller
             'fly'=>$fly,
             'airline'=>$airline
         ));
-        $service_email->sendEmail('Solicitud de Transfer', $touristEmail,'Solicitud de Transfer', 'services@mycasaparticular.com ', $content);
-        $service_email->sendEmail('Solicitud de Transfer', $touristEmail,'Solicitud de Transfer', 'vhagar91@gmail.com ', $content);
+        $service_email->sendEmail('Solicitud de Transfer', 'no-reply@mycasaparticular.com','Solicitud de Transfer', 'services@mycasaparticular.com ', $content);
+        $service_email->sendEmail('Solicitud de Transfer', 'no-reply@mycasaparticular.com','Solicitud de Transfer', 'vhagar91@gmail.com ', $content);
 
         $message = $this->get('translator')->trans("USER_CONTACT_TOURIST_SUCCESS");
         $this->get('session')->getFlashBag()->add('message_global_success', $message);
